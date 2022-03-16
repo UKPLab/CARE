591c4cf6b87146137f6691e3d431ea95041c969d
Dennis Zyska <dennis@zyska.org>
2022-03-16 13:22:29 +0100

    add upload route and pdfjs submodule

--- content-server/frameworks/hypothesis/client/src/shared/messaging/index.js
+++
@@ -1,4 +0,0 @@
-export { PortFinder } from './port-finder';
-export { PortProvider } from './port-provider';
-export { PortRPC, installPortCloseWorkaroundForSafari } from './port-rpc';
-export { isMessageEqual } from './port-util';
