16d0c6a79e53343c96319cfe71d5fbdf0c161c92
Dennis Zyska <dennis@zyska.org>
2022-04-21 13:41:17 +0200

    added upload as additional vue component

--- content-server/frontend/src/App.vue
+++ content-server/frontend/src/App.vue
@@ -1,41 +1,12 @@
 <script>
-import axios from "axios";
+import Upload from "./components/Upload.vue";
 
 export default {
+  components: {Upload},
   data() {
     return {
-      page_title: "TEST_TITLE",
-      page_subtitle: "TEST_SUBTITLE"
-    }
-  },
-  methods: {
-    upload() {
-        let fileElement = document.getElementById('fileInput')
-
-      // check if user had selected a file
-      if (fileElement.files.length === 0) {
-        alert('please choose a file')
-        return
-      }
-
-      let formData = new FormData();
-      formData.set('file', fileElement.files[0]);
-
-      axios.post(
-          "/api/upload",
-          formData,
-          {
-                onUploadProgress: progressEvent => {
-                    const percentCompleted = Math.round(
-                        (progressEvent.loaded * 100) / progressEvent.total
-                    );
-                console.log(`upload process: ${percentCompleted}%`);
-            }
-      }).then(res => {
-          console.log(res.data)
-          console.log(res.data.id)
-          window.location.href = `/annotate/${res.data.id}`;
-      })
+      page_title: "Peer",
+      page_subtitle: "Annotation-based peer review"
     }
   }
 }
@@ -59,10 +30,7 @@ export default {
                   <img src="./assets/images/logo.svg" alt="Logo">
                 </div>
 
-                <div class="w3-container w3-section">
-                    <input class="w3-input w3-border w3-round w3-margin" id="fileInput" type="file" name="file"/>
-                    <button class="w3-btn w3-blue w3-margin" id="uploadButton" @click="upload">upload</button>
-                </div>
+               <Upload></Upload>
             </div>
         </div>
     </main>
--- content-server/frontend/src/main.js
+++ content-server/frontend/src/main.js
@@ -1,6 +1,5 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
-
 
 
 createApp(App).mount('#app')
