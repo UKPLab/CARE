123fecd0dede10a49dd623b74342e40d6b0e8f0a
Nils Dycke <n.dycke@gmx.de>
2022-05-02 09:40:26 +0200

    Running websocket connection.

--- content-server/frontend/src/App.vue
+++ content-server/frontend/src/App.vue
@@ -1,4 +1,26 @@
 <script>
+  import SocketStore from './vuex-store.js'
+  import {mapGetters} from "vuex"
+
+  export default {
+    created() {
+      this.$socket.on('connect', (data) => { console.log('socket connected') });
+    },
+    computed: {},
+    sockets: {
+      connect: function () {
+        console.log('socket connected')
+      }
+    },
+   mounted() {
+     this.$socket.emit("push_pdf", {"key": "value"});
+
+     this.sockets.subscribe("topic", data => {
+       console.log("Received message on topic");
+       console.log(data);
+     });
+   }
+  }
 </script>
 
 <template>
