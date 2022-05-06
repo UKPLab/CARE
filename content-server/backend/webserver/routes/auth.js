801c9fd6d939aaed0f566ad2dafa0176cdfc2ed9
Nils Dycke <dycke@ukp.informatik.tu-darmstadt.de>
2022-05-06 12:17:41 +0200

    Added additional, required middlewares. Still not working, but login triggers a database request and retrieves the right row. Fails during password hash computation ("first argument must be of type string").

--- content-server/backend/webserver/routes/auth.js
+++ content-server/backend/webserver/routes/auth.js
@@ -6,7 +6,7 @@ const pgp = require('pg-promise')();
 
 const router = express.Router();
 
-const pdb = pgp({
+const pdb = pgp({ //todo move to an own module
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
         database: process.env.POSTGRES_PEERDB,
@@ -18,29 +18,53 @@ passport.use(new LocalStrategy(function verify(username, password, cb) {
     console.log(username);
     console.log(password);
 
-  pdb.get('SELECT * FROM "user" WHERE user_name = ? OR email = ?', [ username, username ], function(err, row) {
-      console.log(row);
-    if (err) { return cb(err); }
-    if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+    //return cb(null, false, {message: "incorrect username or password"});
 
-    crypto.pbkdf2(password, row.salt, 310000, 32, 'sha256', function(err, hashedPassword) {
-      if (err) { return cb(err); }
-      if (!crypto.timingSafeEqual(Buffer.from(row.password_hash, 'hex'), hashedPassword)) {
-        return cb(null, false, { message: 'Incorrect username or password.' });
-      }
-      return cb(null, row);
-    });
-  });
+    pdb.query('SELECT * FROM public."user" WHERE "user_name" = $1 OR "email" = $2', [ username, password ])
+        .then((row) => {
+            console.log(row);
+
+            if (!row) { return cb(null, false, { message: 'Incorrect username or password.' }); }
+
+            console.log(password);
+
+            crypto.pbkdf2(password, Buffer.from(row.salt, "hex"), 310000, 32, 'sha256', function(err, hashedPassword) {
+              if (err) { return cb(err); }
+
+              console.log("inside key compute");
+
+              if (!crypto.timingSafeEqual(Buffer.from(row.password_hash, 'hex'), hashedPassword)) {
+                return cb(null, false, { message: 'Incorrect username or password.' });
+              }
+              return cb(null, row);
+            });
+        })
+        .catch((err) => {
+            return cb(err);
+        })
+
 }));
 
+/* not necessary apparently, but typically done...
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
+});
+*/
 
 router.post(
     '/auth/guest_login',
-    passport.authenticate('local', {
-        successRedirect: "/",
-        failureRedirect: '/login',
-    })
+    function(req, res, next){
+        console.log("LOGIN REQUEST ON GUEST");
 
+        passport.authenticate('local', {
+            successRedirect: "/",
+            failureRedirect: '/login'
+        })(req, res, next);
+    }
 );
 
 module.exports = router;
