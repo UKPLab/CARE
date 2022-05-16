4ac14ab64806d77123a0ae5b78cbf70baac5bdc3
Nils Dycke <dycke@ukp.informatik.tu-darmstadt.de>
2022-05-16 11:34:53 +0200

    Started adding user to h database.

--- content-server/backend/tools/db.js
+++ content-server/backend/tools/db.js
@@ -1,10 +1,20 @@
-const crypto = require("crypto");
+const crypto = require('crypto');
 const pgp = require('pg-promise')();
+
 
 exports.pdb = pgp({
         host: process.env.POSTGRES_HOST,
         port: process.env.POSTGRES_PORT,
         database: process.env.POSTGRES_PEERDB,
+        user: "postgres",
+        password: ""
+})
+
+// h database connector
+exports.hdb = pgp({
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        database: "postgres",
         user: "postgres",
         password: ""
 })
@@ -23,6 +33,22 @@ exports.addUser = async function addUser(user_name, user_email, password, role, 
 
     let derivedKey = await createPwd(password, salt);
     derivedKey = derivedKey.toString('hex');
+
+    //create user in h database
+    //TODO ...
+    /*
+    await exports.hdb.query(`
+        INSERT INTO public."user" (username, authority, )
+                VALUES ($1::integer,
+                        $6::character varying,
+                        $2::character varying,
+                        'User'::character varying,
+                        $2,
+                        $3::character varying,
+                        $4::character varying,
+                        $5::character varying)`,
+        [hid, user_name, user_email, derivedKey, salt, role]);
+    )*/
 
     await exports.pdb.query(`
                 INSERT INTO public."user" (hid, sys_role, first_name, last_name, user_name, email, password_hash, salt)
