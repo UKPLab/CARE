5a13ea9bfb9168260282aba2383d894f6367016f
zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-05-19 15:47:28 +0200

    show message for empty list

--- frontend/src/components/dashboard/documents/List.vue
+++ frontend/src/components/dashboard/documents/List.vue
@@ -5,7 +5,10 @@
       <Upload @addedDoc="onAddedDoc"></Upload>
     </div>
     <div class="card-body">
-    <table class="table table-hover">
+      <span v-if="items.length === 0">
+        No Documents available...
+      </span>
+    <table v-else class="table table-hover">
       <thead>
         <tr>
           <th v-for="field in fields" scope="col">{{field.name}}</th>
@@ -29,13 +32,13 @@
                 </svg>
                 <span class="visually-hidden">Delete</span>
               </button>
-            <button type="button" class="btn btn-outline-secondary" @click="renameDoc(item.uid, 'default_name')" data-toggle="tooltip" data-placement="top" title="Rename document...">
+            <!--<button type="button" class="btn btn-outline-secondary" @click="renameDoc(item.uid, 'default_name')" data-toggle="tooltip" data-placement="top" title="Rename document...">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                   <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                   <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                 </svg>
                 <span class="visually-hidden">Rename</span>
-            </button>
+            </button>-->
           </td>
         </tr>
       </tbody>
