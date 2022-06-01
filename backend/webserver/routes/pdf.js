9c0049337b331bcfb076ea430bd6a695b2d2554d
Dennis Zyska <dennis@zyska.org>
2022-06-01 10:15:07 +0200

    change pdf.js directory for better import

--- backend/webserver/routes/pdf.js
+++ backend/webserver/routes/pdf.js
@@ -11,15 +11,10 @@ const express = require('express');
 const connectEnsureLogin = require("connect-ensure-login");
 
 const PDF_PATH = `${__dirname}/../../../files/`;
-const PDFJS_PATH = `${__dirname}/../../assets/pdfjs`;
-
-console.log(PDFJS_PATH);
 
 module.exports = function(app) {
 
     app.use("/pdf/*", connectEnsureLogin.ensureLoggedIn("/login"));
-
-    app.use("/pdfjs-dist", express.static(PDFJS_PATH));
 
     app.get('/pdf/:pdf', (req, res, next) => {
         const data = fs.readFileSync(`${PDF_PATH}${req.params.pdf}.pdf`);
