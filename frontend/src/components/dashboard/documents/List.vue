50c73ff43ad8c2b48085ebb14a592486c983693d
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-06-03 15:35:53 +0200

    pdf.js problem with require fixed

--- frontend/src/components/dashboard/documents/List.vue
+++ frontend/src/components/dashboard/documents/List.vue
@@ -73,8 +73,9 @@ export default {
   methods: {
     ...mapActions({load: "user/load", delete: "user/delete"}),
     accessDoc(docHash) {
-      let routeData = this.$router.resolve(`/annotate/${docHash}`);
-      window.open(routeData.href, '_blank');
+      this.$router.push(`/annotate/${docHash}`);
+      //let routeData = this.$router.resolve(`/annotate/${docHash}`);
+      //window.open(routeData.href, '_blank');
     },
     onAddedDoc(){
       this.load();
