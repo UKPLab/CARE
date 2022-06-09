b3738e82be4a304b24b29e5c074e3e24afe04804
Nils Dycke <dycke@ukp.informatik.tu-darmstadt.de>
2022-06-09 09:24:50 +0200

    Added documentation to building scripts.

--- pyenv.sh
+++ pyenv.sh
@@ -1,3 +1,4 @@
+# Script for sourcing pyenv -- used during setup
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 if command -v pyenv 1>/dev/null 2>&1; then
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
+/* vite.config.js - configure vite for vue
+
+This file is necessary to specify where and how vue components
+are compiled into a build directory. This file configures
+vite to build the components into the ../dist directory.
+
+Author: Dennis Zyska (zyska@ukp...)
+Co-Author:  Nils Dycke (dycke@ukp...)
+Source: https://vitejs.dev/config/
+*/
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
