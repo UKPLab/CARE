a378569a64f6e432afcd55f0227d29707baa7505
Nils Dycke <n.dycke@gmx.de>
2022-06-22 17:14:56 +0200

    Fixed styling of the sidebar. Stay away from the whole flex layout...

--- frontend/src/components/dashboard/annotater/sidebar/Sidebar.vue
+++ frontend/src/components/dashboard/annotater/sidebar/Sidebar.vue
@@ -1,35 +1,16 @@
 <template>
-  <b-container fluid id="sidebar-container">
-    <b-row>
-      <b-col id="collapsable" cols="sm-auto" offset="0">
-        <button @click="toggleSidebar()" type="button" class="btn btn-outline-secondary"  data-toggle="tooltip" data-placement="top" title="Open/close sidebar...">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right-square" viewBox="0 0 16 16">
-          <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
-          <path d="M5.795 12.456A.5.5 0 0 1 5.5 12V4a.5.5 0 0 1 .832-.374l4.5 4a.5.5 0 0 1 0 .748l-4.5 4a.5.5 0 0 1-.537.082z"/>
-        </svg>
-        <span class="visually-hidden">Sidebar</span>
-      </button>
-      </b-col>
-      <b-col id="annotations" offset="0" padding="0" class="collapse collapse-horizontal border-end"  v-bind:class="showing">
-        <b-container style="height:100vh;padding:0px">
-          <b-row id="top-row">
-            <TopBar></TopBar>
-          </b-row>
-          <b-row id="pane-row">
-            <div id="sidepane">
-              <div v-if="annotations.length === 0" class="list-group border-0 rounded-0 text-sm-start min-vh-100">
-                    No annotations present...
-              </div>
-              <div v-else id="anno-list" class="list-group border-0 rounded-0 text-sm-start min-vh-100">
-                <a v-for="anno in annotations" class="list-group-item border-end-0 d-inline-block" data-bs-parent="#sidebar">
-                  <span>{{anno.comment}}</span></a>
-              </div>
-            </div>
-          </b-row>
-        </b-container>
-        </b-col>
-      </b-row>
-  </b-container>
+  <div id="sidebar-container" class="collapse collapse-horizontal border-end d-flex flex-column vh-100" v-bind:class="showing">
+    <TopBar id="topbar"></TopBar>
+    <div id="sidepane">
+        <div v-if="annotations.length === 0" class="list-group border-0 rounded-0 text-sm-start">
+              No annotations present...
+        </div>
+        <div v-else id="anno-list" class="list-group border-0 rounded-0 text-sm-start">
+          <a v-for="anno in annotations" class="list-group-item border-end-0 d-inline-block" data-bs-parent="#sidebar">
+            <span>{{anno.comment}}</span></a>
+        </div>
+      </div>
+  </div>
 </template>
 
 <script>
@@ -60,27 +41,17 @@ export default {
 
 <style scoped>
 #sidebar-container {
-  height:100vh;
+  max-width:300px;
+  height:100%;
+}
+#topbar {
+  width:300px;
 }
 #sidepane {
+  padding-top:5px;
   background-color: #bfbfbf;
-  height:100%;
   overflow:scroll;
-}
-#top-row {
-  height: 10%;
-}
-#pane-row {
-  height: 90%;
-}
-#collapsable {
-  padding-left: 0;
-  padding-right: 0;
-  max-width:10%;
-}
-#annotations {
-  padding: 0;
-  max-width:90%;
-  height:100px;
+  width:100%;
+  height:100%;
 }
 </style>
--- frontend/src/components/dashboard/annotater/sidebar/TopBar.vue
+++ frontend/src/components/dashboard/annotater/sidebar/TopBar.vue
@@ -1,13 +1,8 @@
 <template>
-<nav id="topbar" class="navbar navbar-expand-lg navbar-light bg-light">
+<nav id="top-navbar" class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">PEER Topbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- check for additional fields: https://getbootstrap.com/docs/5.0/components/navbar/ -->
-    </div>
+    <!-- check for additional fields: https://getbootstrap.com/docs/5.0/components/navbar/ -->
   </div>
 </nav>
 </template>
@@ -19,8 +14,7 @@ export default {
 </script>
 
 <style scoped>
-#topbar {
-  max-width:100%;
-  width:500px;
+#top-navbar {
+  width:100%;
 }
 </style>
