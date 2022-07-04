2dc70b3fc47cd0f3c60c856fae0654d290f3d8d9
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-07-04 16:28:02 +0200

    added PDF Page (still errors)

---
+++ frontend/src/components/dashboard/annotater/pdfViewer/PDFPage.vue
@@ -0,0 +1,148 @@
+<script>
+import debug from 'debug';
+const log = debug('app:components/PDFPage');
+export const PIXEL_RATIO = window.devicePixelRatio || 1;
+export const VIEWPORT_RATIO = 0.98;
+import { h } from 'vue'
+
+export default {
+  name: 'PDFPage',
+  props: {
+    page: {
+      type: Object, // instance of PDFPageProxy returned from pdf.getPage
+      required: true,
+    },
+    scale: {
+      type: Number,
+      required: true,
+    },
+    scrollTop: {
+      type: Number,
+      default: 0
+    },
+    clientHeight: {
+      type: Number,
+      default: 0
+    },
+  },
+  data() {
+    return {
+      elementTop: 0,
+      elementHeight: 0,
+    };
+  },
+  computed: {
+    actualSizeViewport() {
+      return this.viewport.clone({scale: this.scale});
+    },
+    canvasStyle() {
+      const {width: actualSizeWidth, height: actualSizeHeight} = this.actualSizeViewport;
+      const [pixelWidth, pixelHeight] = [actualSizeWidth, actualSizeHeight]
+        .map(dim => Math.ceil(dim / PIXEL_RATIO));
+      return `width: ${pixelWidth}px; height: ${pixelHeight}px;`;
+    },
+    canvasAttrs() {
+      let {width, height} = this.viewport;
+      [width, height] = [width, height].map(dim => Math.ceil(dim));
+      const style = this.canvasStyle;
+      return {
+        width,
+        height,
+        style,
+        class: 'pdf-page box-shadow',
+      };
+    },
+    pageNumber() {
+      return this.page.pageNumber;
+    },
+    isElementVisible() {
+      const {elementTop, elementBottom, scrollTop, scrollBottom} = this;
+      if (!elementBottom) return;
+      return elementTop < scrollBottom && elementBottom > scrollTop;
+    },
+    elementBottom() {
+      return this.elementTop + this.elementHeight;
+    },
+    scrollBottom() {
+      return this.scrollTop + this.clientHeight;
+    },
+  },
+  methods: {
+    updateElementBounds() {
+      const {offsetTop, offsetHeight} = this.$el;
+      this.elementTop = offsetTop;
+      this.elementHeight = offsetHeight;
+    },
+    drawPage() {
+      if (this.renderTask) return;
+      const {viewport} = this;
+      const canvasContext = this.$el.getContext('2d');
+      const renderContext = {canvasContext, viewport};
+      // PDFPageProxy#render
+      // https://mozilla.github.io/pdf.js/api/draft/PDFPageProxy.html
+      this.renderTask = this.page.render(renderContext);
+      this.renderTask
+        .then(() => {
+          this.$emit('page-rendered', {
+            page: this.page,
+            text: `Rendered page ${this.pageNumber}`,
+          });
+         })
+        .catch(response => {
+          this.destroyRenderTask();
+          this.$emit('page-errored', {
+            response,
+            page: this.page,
+            text: `Failed to render page ${this.pageNumber}`,
+          });
+        });
+    },
+    destroyPage(page) {
+      // PDFPageProxy#_destroy
+      // https://mozilla.github.io/pdf.js/api/draft/PDFPageProxy.html
+      if (page) page._destroy();
+      this.destroyRenderTask();
+    },
+    destroyRenderTask() {
+      if (!this.renderTask) return;
+      // RenderTask#cancel
+      // https://mozilla.github.io/pdf.js/api/draft/RenderTask.html
+      this.renderTask.cancel();
+      delete this.renderTask;
+    },
+  },
+  watch: {
+    scale: 'updateElementBounds',
+    scrollTop: 'updateElementBounds',
+    clientHeight: 'updateElementBounds',
+    page(_newPage, oldPage) {
+      this.destroyPage(oldPage);
+    },
+    isElementVisible(isElementVisible) {
+      if (isElementVisible) this.drawPage();
+    },
+  },
+  created() {
+    // PDFPageProxy#getViewport
+    // https://mozilla.github.io/pdf.js/api/draft/PDFPageProxy.html
+    this.viewport = this.page.getViewport(this.scale);
+  },
+  mounted() {
+    log(`Page ${this.pageNumber} mounted`);
+    this.updateElementBounds();
+  },
+  beforeDestroy() {
+    this.destroyPage(this.page);
+  },
+  render() {
+    const {canvasAttrs: attrs} = this;
+    return h('canvas', {attrs});
+  },
+};
+</script>
+<style>
+.pdf-page {
+  display: block;
+  margin: 0 auto;
+}
+</style>
