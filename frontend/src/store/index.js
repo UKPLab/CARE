7430610888373684a55154415f3b1b483f50e6c7
Dennis Zyska <dennis@zyska.org>
2022-07-11 14:06:07 +0200

    cleanup and removed unsused websocket store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -15,7 +15,6 @@ Source: -
 */
 import { createStore } from 'vuex';
 import AuthStore from './modules/auth.js';
-import WebsocketStore from './modules/websocket.js';
 import UserStore from './modules/user.js';
 import AnnotationStore from './modules/anno.js';
 import createPersistedState from 'vuex-persistedstate';
@@ -24,7 +23,6 @@ import createPersistedState from 'vuex-persistedstate';
 export default createStore({
     modules: {
         auth: AuthStore,
-        websocket: WebsocketStore,
         user: UserStore,
         anno: AnnotationStore,
     },
