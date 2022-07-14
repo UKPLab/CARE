00a3f54bab620db4e7cb86d89fffec1d3979ac7d
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-07-14 10:07:28 +0200

    added websocket logger

---
+++ backend/webserver/sockets/log.js
@@ -0,0 +1,21 @@
+const logger = require("../../utils/logger.js")("sockets/log");
+
+exports = module.exports = function (io) {
+
+    io.on("connection", (socket) => {
+
+        const new_logger = logger.child({user: socket.request.session.passport.user.id})
+
+        socket.on("log", (data) => {
+            try {
+                if (data.meta) {
+                    new_logger.log(data.level, data.message, data.meta);
+                } else {
+                    new_logger.log(data.level, data.message);
+                }
+            } catch(e) {
+                new_logger.error("Can't log message: " + JSON.stringify(data));
+            }
+        });
+    });
+}
