81c6c40ec5dafeaae024f070e94593ee2e4b9572
Dennis Zyska <dennis@zyska.org>
2022-07-21 16:30:40 +0200

    add db migration for table review

---
+++ backend/db/migrations/20220721125009-create-review.js
@@ -0,0 +1,67 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('review',
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER
+                },
+                startAt: {
+                    allowNull: false, type: Sequelize.DATE
+                },
+                startBy: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    references: {
+                        model: "user", key: "id"
+                    }
+                },
+                document: {
+                type: Sequelize.STRING,
+                references: {
+                    model: "document",
+                    key: "hash"
+                }
+                },
+                hash: {
+                type: Sequelize.STRING, allowNull: false, unique: true
+            },
+                submitAt: {
+                    allowNull: true, type: Sequelize.DATE
+                },
+                decisionBy: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    references: {
+                        model: "user", key: "id"
+                    }
+                },
+                decisionAt: {
+                    allowNull: true, type: Sequelize.DATE
+                },
+                decisionReason: {
+                    type: Sequelize.STRING
+                },
+                submitted: {
+                    type: Sequelize.BOOLEAN,
+                    default: false
+                },
+                accepted: {
+                    type: Sequelize.BOOLEAN,
+                    default: false
+                },
+                createdAt: {
+                    allowNull: false, type: Sequelize.DATE
+                }, updatedAt: {
+                    allowNull: false, type: Sequelize.DATE
+                }
+            });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('review');
+  }
+};
