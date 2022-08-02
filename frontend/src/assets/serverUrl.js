687f59aa5aaa9c105013bf39cf6a861597fa3872
Dennis Zyska <dennis@zyska.org>
2022-08-02 21:02:51 +0200

    change url to connect

---
+++ frontend/src/assets/serverUrl.js
@@ -0,0 +1,9 @@
+const getServerURL = () => {
+  if (process.env.NODE_ENV !== 'production') {
+      return import.meta.env.VITE_APP_SERVER_URL;
+    } else {
+      return window.location.protocol + "//" + window.location.host;
+  }
+};
+
+export default getServerURL;
