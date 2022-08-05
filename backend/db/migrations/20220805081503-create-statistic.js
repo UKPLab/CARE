bc6c23c320907ace03b0749103e12a51ed19f15e
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-08-05 12:25:25 +0200

    added stats table and commands

---
+++ backend/db/migrations/20220805081503-create-statistic.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('statistic',
+            {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: Sequelize.INTEGER
+                },
+                action: {
+                    type: Sequelize.STRING,
+                    allowNull: false
+                },
+                data: {
+                    type: Sequelize.STRING
+                },
+                user: {
+                    type: Sequelize.INTEGER,
+                    allowNull: true,
+                    references: {
+                        model: "user", key: "id"
+                    }
+                },
+                timestamp: {
+                    type: Sequelize.DATE, allowNull: false
+                },
+                createdAt: {
+                    allowNull: false, type: Sequelize.DATE
+                },
+                updatedAt: {
+                    allowNull: false, type: Sequelize.DATE
+                }
+            });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('statistic');
+  }
+};
