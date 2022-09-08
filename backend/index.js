aca77e7ae77cdda8c996f8e9b60886ee9a743a0f
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-09-08 21:08:14 +0200

    close open connections

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const port = process.env.CONTENT_SERVER_PORT || 3001;
 
 const webserver = require("./webserver/webServer.js");
+const {useSsl} = require("./webserver/createServer");
 
-module.exports = webserver({port: port});
+const app = webserver();
+const logger = require("./utils/logger.js")( "index");
+
+// serve server on port
+app.listen(port, () => {
+    const scheme = useSsl ? 'https' : 'http';
+    logger.info(`Web Server started at ${scheme}://localhost:${port}/`)
+});
--- backend/tests/login.test.js
+++ backend/tests/login.test.js
@@ -1,6 +1,11 @@
 const request = require('supertest')
-const app = require('../index.js')
+const webserver = require('../webserver/webServer.js')
+const db = require("../db/index.js")
+
+const app = webserver();
+
 describe('Post Endpoints', () => {
+
     it('should create a new post', async () => {
         const res = await request(app)
             .post('/auth/login')
@@ -12,4 +17,8 @@ describe('Post Endpoints', () => {
         expect(res.statusCode).toEqual(200)
         //expect(res.body).toHaveProperty('post')
     })
+
+    afterAll(() => {
+        db.sequelize.close();
+    });
 })
