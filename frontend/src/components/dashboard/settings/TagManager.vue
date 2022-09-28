60d21c90d6b472dbc7dfcab293fca1d730f5e2e9
Dennis Zyska <dennis@zyska.org>
2022-09-28 13:41:39 +0200

    added table with tagset information

--- frontend/src/components/dashboard/settings/TagManager.vue
+++ frontend/src/components/dashboard/settings/TagManager.vue
@@ -2,50 +2,74 @@
   <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
       Tag Sets
-      <button class="btn btn-primary" @click="addTagSet">Add Tag Set</button>
+      <button class="btn btn-primary btn-sm" @click="addTagSet">Add new tag set</button>
     </div>
 
-    <div class="card-body p-4">
-      <Loader v-if="items === null" :loading="items === null" text="Collecting data..."></Loader>
+    <div class="card-body p-2">
+      <Loader v-if="tagSets === null || tags === null" :loading="tagSets === null" text="Collecting data..."></Loader>
 
-      <span v-else-if="items.length === 0">
+      <span v-else-if="tagSets.length === 0">
           No tag sets are available...
         </span>
       <table v-else class="table table-hover">
         <thead>
         <tr>
-          <th v-for="field in fields" scope="col">{{ field.name }}</th>
-          <th scope="col">Manage</th>
-          <th scope="col">Review</th>
+          <th scope="col">Name</th>
+          <th scope="col">Created</th>
+          <th scope="col">Last change</th>
+          <th scope="col">Public</th>
+          <th scope="col">User</th>
+          <th scope="col">#Tags</th>
+          <th scope="col">Actions</th>
         </tr>
         </thead>
         <tbody>
-        <tr v-for="item in items" :key="item.hash">
-          <td v-for="field in fields">{{ item[field.col] }}</td>
+        <tr v-for="tagSet in tagSets" :key="tagSet.id">
+          <td>{{ tagSet['name'] }}</td>
+          <td>{{ new Date(tagSet['createdAt']).toLocaleString("en-US") }}</td>
+          <td>{{ new Date(tagSet['updatedAt']).toLocaleString("en-US") }}</td>
           <td>
-            <div class="btn-group">
-              <button class="btn btn-outline-secondary" data-placement="top" data-toggle="tooltip"
-                      title="Delete document..."
-                      type="button" @click="deleteDoc(item.id)">
-                <svg class="bi bi-trash3-fill" fill="currentColor" height="16" viewBox="0 0 16 16"
-                     width="16" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                      d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z"/>
-                </svg>
-                <span class="visually-hidden">Delete</span>
-              </button>
+            <div v-if="tagSet['public'] || tagSet['userId'] === null" class="badge bg-success">Yes</div>
+            <div v-else class="badge bg-danger">No</div>
+          </td>
+          <td>
+            <div v-if="tagSet['userId'] === null" class="badge bg-black">System</div>
+            <div v-else class="badge bg-black">{{ tagSet['userId'] }}</div>
+          </td>
+          <td>
+            <div :title="tags.filter(tag => tag.setId === tagSet.id).map(e => e.name).join('<br>')" class="badge bg-primary" data-bs-html="true"  data-bs-placement="top" data-bs-toggle="tooltip"
+                 role="button">
+              {{ tags.filter(tag => tag.setId === tagSet.id).length }}
             </div>
           </td>
           <td>
-            <button :class="reviewState(item.hash) === 'SUBMITTED' ? 'disabled btn-outline-secondary' : reviewState(item.hash) === 'PENDING' ? 'btn-outline-primary' : 'btn-outline-success'"
-                    class="btn"
-                    type="button"
-                    @click="startReview(item.hash)">{{
-                reviewState(item.hash) === "PENDING" ? "Continue"
-                    : (reviewState(item.hash) === "SUBMITTED" ? "Submitted" : "Start")
-              }}
-            </button>
+            <div class="btn-group  btn-group-sm" role="group" aria-label="Basic example">
+              <button class="btn btn-outline-dark btn-sm" @click="editTagSet(tagSet.id)">
+                <svg class="bi bi-pencil" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
+                </svg>
+              </button>
+              <button class="btn btn-outline-dark btn-sm" @click="deleteTagSet(tagSet.id)">
+                <svg class="bi bi-trash" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                        fill-rule="evenodd"/>
+                </svg>
+              </button>
+              <button class="btn btn-outline-dark btn-sm" @click="shareTagSet(tagSet.id)">
+                <svg class="bi bi-share" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
+                     xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z"/>
+                </svg>
+              </button>
+            </div>
           </td>
+
         </tr>
         </tbody>
       </table>
@@ -56,7 +80,7 @@
 <script>
 import {mapGetters, mapActions} from "vuex";
 import Loader from "../../general/Loader.vue";
-
+import {Tooltip} from "bootstrap";
 
 export default {
   name: "TagManager",
@@ -64,10 +88,9 @@ export default {
   data() {
     return {
       fields: [
-        {name: "Title", col: "name"},
+        {name: "Name", col: "name"},
         {name: "Created At", col: "createdAt"}
       ],
-      loading: true,
     }
   },
   props: {
@@ -78,12 +101,26 @@ export default {
   },
   mounted() {
     this.$socket.emit("getTagSets");
+    this.$socket.emit("getTags");
   },
   computed: {
-    ...mapGetters({items: 'tag/getTagSets'})
+    ...mapGetters({tagSets: 'tag/getTagSets', tags: 'tag/getTags'}),
+  },
+  watch: {
+    tagSets: function (newVal, oldVal) {
+      new Tooltip(document.body, {
+        selector: "[data-bs-toggle='tooltip']",
+      })
+    },
+    tags: function (newVal, oldVal) {
+      new Tooltip(document.body, {
+        selector: "[data-bs-toggle='tooltip']",
+      })
+    }
   },
   methods: {
     addTagSet() {
+      console.log(this.tags);
       this.$socket.emit("addTagSet", {name: "New Tag Set"});
     },
   }
