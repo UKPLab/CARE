8f63317636caa618ea9c8736d57c8576d0391cf0
Nils Dycke <n.dycke@gmx.de>
2022-10-06 08:58:46 +0200

    Authentification check + simple test (again needs verification, as some async stuff is going on).

--- backend/tests/annotation.export.test.js
+++ backend/tests/annotation.export.test.js
@@ -10,13 +10,6 @@ describe('Annotation Exporting Test', () => {
         [app, httpServer] = webserver();
         httpServer.listen(3001, () => console.log("Started test server"));
 
-        // setup ioClient
-        ioClient = io_client(`http://localhost:3001`);
-        ioClient.on("toast", (data) => {
-            expect(false, `server returned an error code: ${data}`);
-        });
-        ioClient.on("connect", done);
-
         //login as admin
         request.post(
             'http://localhost:3001/auth/login',
@@ -27,6 +20,27 @@ describe('Annotation Exporting Test', () => {
             },
             function (error, response, body) {
                 if (!error && response.statusCode === 200) {
+                    const sid = response.headers["set-cookie"][0];
+
+                    var opts = {
+                        transportOptions: {
+                            polling: {
+                                extraHeaders: {
+                                    'Cookie': sid
+                                }
+                            }
+                        },
+                        withCredentials: true
+                    };
+
+                    // setup ioClient
+                    ioClient = io_client(`http://localhost:3001`, opts);
+                    ioClient.on("toast", (data) => {
+                        expect(false, `server returned an error code: ${data}`);
+                    });
+                    ioClient.on("connect", done);
+
+                    //connect
                     ioClient.connect();
                 } else {
                     throw Error(`Cannot start test due to failed login. Error msg: ${body}`);
@@ -35,17 +49,20 @@ describe('Annotation Exporting Test', () => {
         );
     });
 
-    it('expects the exported annotations', async () => {
+    it('expects the exported annotations', (done) => {
         ioClient.on("exportedAnnotations", (data) => {
             expect("success" in data && data["success"]);
             expect("csvs" in data && "docids" in data);
 
             const blobs = data["csvs"];
             const docids = data["docids"];
+
+            done();
         });
         ioClient.emit("exportAnnotations", ["8852a746-360e-4c31-add2-4d1c75bfb96d"]);
     });
 
     afterAll(() => {
+        ioClient.close()
     });
 })
