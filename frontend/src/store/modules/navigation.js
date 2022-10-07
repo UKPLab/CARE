b60e01b97b6eee67b2e250a75f1a06f5577ee8c1
Dennis Zyska <dennis@zyska.org>
2022-10-07 12:41:30 +0200

    added getter and simple loop inside sidebar

--- frontend/src/store/modules/navigation.js
+++ frontend/src/store/modules/navigation.js
@@ -8,15 +8,15 @@ Source: -
 const getDefaultState = () => {
     return {
         groups: [
-            {'id': 0, 'name': "Default", 'icon': 'IconFan',},
-            {'id': 1, 'name': "Admin", "access": ['admin']}
+            {'id': 1, 'name': "Default", 'icon': 'IconFan',},
+            {'id': 2, 'name': "Admin", "access": ['admin']}
         ],
         elements: [
-            {'name': "Dashboard", 'group': 0, 'icon': 'IconFan', default:true, "order": 0, "path": "home", 'component': 'Home', "alias": ['/', "/index.html"]},
-            {'name': "Documents", 'group': 0, 'icon': 'IconFan', "order": 10, "path": "documents", 'component': 'Home'},
-            {'name': "Reviews", 'group': 0, "path": "reviews", 'component': 'Home'},
-            {'name': "Meta Reviews", 'group': 0, "access": ["admin"], "path": "meta_reviews", 'component': 'Home'},
-            {'name': "Tags", 'group': 0, 'icon': 'IconFan',"path": "tags", 'component': 'Home'},
+            {'name': "Dashboard", 'icon': 'IconFan', default:true, "order": 0, "path": "home", 'component': 'Home', "alias": ['/', "/index.html"]},
+            {'name': "Documents", 'group': 1, 'icon': 'IconFan', "order": 10, "path": "documents", 'component': 'Home'},
+            {'name': "Reviews", 'group': 1, "path": "reviews", 'component': 'Home'},
+            {'name': "Meta Reviews", 'group': 2, "access": ["admin"], "path": "meta_reviews", 'component': 'Home'},
+            {'name': "Tags", 'group': 1, 'icon': 'IconFan',"path": "tags", 'component': 'Home'},
             {'name': "Settings", 'group': 1, "path": "settings", 'component': 'Home'}
     ]
     };
@@ -29,7 +29,28 @@ export default {
     getters: {
         getNavElements: state => {
             return state["elements"]
-        }
+        },
+        getSidebarElements: state => {
+            //Group by group value
+            return state["elements"].reduce((acc, cur) => {
+                if (cur["group"] !== undefined) {
+                    acc[cur["group"]] = acc[cur["group"]] || [];
+                    acc[cur['group']].push(cur)
+                } else {
+                    acc[0] = acc[0] || [];
+                    acc[0].push(cur)
+                }
+                return acc
+            }, [])
+            //TODO sorting by order inside groups
+        },
+        getSidebarGroups: state => {
+            // Order by order value
+            return state["groups"].sort(function(a, b) {
+                return a["order"] - b["order"];
+            });
+        },
+
     },
     mutations: {
     },
