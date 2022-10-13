f075744dc7e46d1fbee819dc9971321c0eebcbe9
Dennis Zyska <dennis@zyska.org>
2022-10-13 12:34:34 +0200

    frontend ability to copy existing tagset to new one

--- frontend/src/components/dashboard/Tags.vue
+++ frontend/src/components/dashboard/Tags.vue
@@ -1,12 +1,12 @@
 <template>
 
-    <TagSetModal ref="tagSetModal" id="0" ></TagSetModal>
+  <TagSetModal id="0" ref="tagSetModal"></TagSetModal>
 
 
-  <div class="card" >
+  <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
       Tag Sets
-      <button class="btn btn-primary btn-sm" v-on:click="this.$refs.tagSetModal.open(0)">Add new tag set</button>
+      <button class="btn btn-primary btn-sm" v-on:click="this.$refs.tagSetModal.new()">Add new tag set</button>
     </div>
 
     <div class="card-body p-2">
@@ -15,7 +15,7 @@
       <span v-else-if="tagSets.length === 0">
           No tag sets are available...
         </span>
-      <table v-else class="table table-hover" ref="tagsTable">
+      <table v-else ref="tagsTable" class="table table-hover">
         <thead>
         <tr>
           <th scope="col">Name</th>
@@ -41,14 +41,16 @@
             <div v-else class="badge bg-black">{{ tagSet['userId'] }}</div>
           </td>
           <td>
-            <div v-tooltip :title="tags.filter(tag => tag.setId === tagSet.id).map(e => e.name).join('<br>')" class="badge bg-primary" data-bs-html="true"  data-bs-placement="top" data-bs-toggle="tooltip"
+            <div v-tooltip :title="tags.filter(tag => tag.setId === tagSet.id).map(e => e.name).join('<br>')"
+                 class="badge bg-primary" data-bs-html="true" data-bs-placement="top" data-bs-toggle="tooltip"
                  role="button">
               {{ tags.filter(tag => tag.setId === tagSet.id).length }}
             </div>
           </td>
           <td>
-            <div class="btn-group  btn-group-sm" role="group" aria-label="Basic example">
-              <button class="btn btn-outline-dark btn-sm" v-on:click="this.$refs.tagSetModal.open(tagSet.id)" :disabled="tagSet['userId'] === userId">
+            <div aria-label="Basic example" class="btn-group  btn-group-sm" role="group">
+              <button :disabled="tagSet['userId'] === userId" class="btn btn-outline-dark btn-sm"
+                      v-on:click="this.$refs.tagSetModal.edit(tagSet.id)">
                 <svg class="bi bi-pencil" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
                      xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -60,17 +62,21 @@
                      xmlns="http://www.w3.org/2000/svg">
                   <path
                       d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
-                        fill-rule="evenodd"/>
+                  <path
+                      d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                      fill-rule="evenodd"/>
                 </svg>
               </button>
-              <button class="btn btn-outline-dark btn-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
-  <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-  <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-</svg>
+              <button class="btn btn-outline-dark btn-sm" v-on:click="this.$refs.tagSetModal.copy(tagSet.id)">
+                <svg class="bi bi-clipboard" fill="currentColor" height="16" viewBox="0 0 16 16"
+                     width="16" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                      d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                  <path
+                      d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+                </svg>
               </button>
-              <button v-tooltip title="test" class="btn btn-outline-dark btn-sm" @click="shareTagSet(tagSet.id)">
+              <button v-tooltip class="btn btn-outline-dark btn-sm" title="test" @click="shareTagSet(tagSet.id)">
                 <svg class="bi bi-share" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
                      xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -93,7 +99,7 @@
 import {mapGetters} from "vuex";
 import Loader from "../general/Loader.vue";
 import TagSetModal from "./tags/TagSetModal.vue";
-import { tooltip } from "../../assets/tooltip.js";
+import {tooltip} from "../../assets/tooltip.js";
 
 export default {
   name: "Tags",
