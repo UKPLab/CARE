6fc94ba7468aa70ae588a9738f6028ae33dd81e1
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-10-17 12:47:06 +0200

    added logic for publish/delete (still some bugs)

--- frontend/src/components/dashboard/Tags.vue
+++ frontend/src/components/dashboard/Tags.vue
@@ -1,7 +1,8 @@
 <template>
 
   <TagSetModal id="0" ref="tagSetModal"></TagSetModal>
-
+  <TagSetDeleteModal  ref="tagSetDeleteModal"></TagSetDeleteModal>
+  <TagSetPublishModal  ref="tagSetPublishModal"></TagSetPublishModal>
 
   <div class="card">
     <div class="card-header d-flex justify-content-between align-items-center">
@@ -56,7 +57,9 @@
                       d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"/>
                 </svg>
               </button>
-              <button v-tooltip title="Delete" v-if="tagSet.userId === userId || isAdmin" class="btn btn-outline-dark btn-sm" @click="deleteTagSet(tagSet.id)">
+              <button v-tooltip title="Delete" v-if="tagSet.userId === userId || isAdmin"
+                      v-on:click="this.$refs.tagSetDeleteModal.open(tagSet.id)"
+                      class="btn btn-outline-dark btn-sm" @click="deleteTagSet(tagSet.id)">
                 <svg class="bi bi-trash" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
                      xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -75,7 +78,9 @@
                       d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
                 </svg>
               </button>
-              <button v-if="!tagSet.publish && (tagSet.userId === userId || isAdmin)" v-tooltip title="Share" class="btn btn-outline-dark btn-sm"  @click="shareTagSet(tagSet.id)">
+              <button v-if="!tagSet.publish && (tagSet.userId === userId || isAdmin)" v-tooltip title="Share"
+                      v-on:click="this.$refs.tagSetPublishModal.open(tagSet.id)"
+                      class="btn btn-outline-dark btn-sm"  @click="shareTagSet(tagSet.id)">
                 <svg class="bi bi-share" fill="currentColor" height="12" viewBox="0 0 16 16" width="12"
                      xmlns="http://www.w3.org/2000/svg">
                   <path
@@ -98,11 +103,13 @@
 import {mapGetters} from "vuex";
 import Loader from "../general/Loader.vue";
 import TagSetModal from "./tags/TagSetModal.vue";
+import TagSetPublishModal from "./tags/TagSetPublishModal.vue";
+import TagSetDeleteModal from "./tags/TagSetDeleteModal.vue";
 import {tooltip} from "../../assets/tooltip.js";
 
 export default {
   name: "Tags",
-  components: {TagSetModal, Loader},
+  components: {TagSetModal, Loader, TagSetPublishModal, TagSetDeleteModal},
   directives: {
     tooltip
   },
@@ -123,11 +130,6 @@ export default {
   computed: {
     ...mapGetters({tagSets: 'tag/getTagSets', tags: 'tag/getAllTags', userId: 'auth/getUserId', isAdmin: 'auth/isAdmin'}),
   },
-  methods: {
-    addTagSet() {
-      this.$socket.emit("addTagSet", {name: "New Tag Set"});
-    },
-  }
 }
 </script>
 
