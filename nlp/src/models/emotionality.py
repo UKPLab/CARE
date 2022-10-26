331802c20dc5132f68735373451399b68e2e2d39
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-10-26 14:45:03 +0200

    removed unused files

--- nlp/src/models/emotionality.py
+++
@@ -1,8 +0,0 @@
-from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from nltk import tokenize
-
-
-def classify(sentence):
-    sentim_analyzer = SentimentIntensityAnalyzer()
-
-    return sentim_analyzer.polarity_scores(sentence)
