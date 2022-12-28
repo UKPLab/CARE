56a8d8fdbde4e6b9876c9df9a510517103e8de93
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2022-12-28 14:07:17 +0100

    update tests

--- backend/db/migrations/20220706134141-basic-users.js
+++ backend/db/migrations/20220706134141-basic-users.js
@@ -42,7 +42,7 @@ module.exports = {
 
     async down(queryInterface, Sequelize) {
         await queryInterface.bulkDelete("user", {
-            user_name: ['admin', 'guest']
+            userName: ['admin', 'guest']
         }, {})
     }
 };
