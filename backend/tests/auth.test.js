2edde8fae6693489e9fb2cba64991b9d209489d7
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2023-01-24 09:31:54 +0100

    refactor backend testing framework

---
+++ backend/tests/auth.test.js
@@ -0,0 +1,69 @@
+const request = require('supertest')
+const Server = require("../webserver/Server.js");
+
+describe('Test Login', () => {
+
+    beforeAll(() => {
+        this.server = new Server();
+        this.server.start(3011);
+    });
+
+    /**
+     * Test the /POST login route with valid credentials
+     */
+    it('Admin Login', async () => {
+        return request(this.server.app)
+            .post('/auth/login')
+            .send({
+                username: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PWD,
+            }).expect(200);
+        //expect(res.statusCode).toEqual(200)
+    })
+
+    /**
+     * Test the /POST login route with invalid credentials
+     */
+    it('Invalid Login', async () => {
+        return request(this.server.app)
+            .post('/auth/login')
+            .send({
+                username: process.env.ADMIN_EMAIL,
+                password: 'invalid',
+            }).expect(401);
+    });
+
+    /**
+     * Test the /GET config route
+     */
+    it('Get Config', async () => {
+        return request(this.server.app)
+            .get('/config.js')
+            .expect(200);
+        // TODO additional check if config holds some values
+    });
+
+    /**
+     * Test the /POST register route
+     */
+    it('Register', async () => {
+        const register = await request(this.server.app)
+            .post('/auth/register')
+            .send({
+                user_name: 'testuser',
+                password: 'testuser',
+                email: 'test@test.de',
+                first_name: 'test',
+                last_name: 'user',
+                terms: true,
+            });
+        expect(register.statusCode).toEqual(201);
+        const login = await request(this.server.app)
+            .post('/auth/login')
+            .send({
+                username: 'testuser',
+                password: 'testuser',
+            });
+        expect(login.statusCode).toEqual(200);
+    });
+})
---
+++ backend/tests/socket.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest')
+const Init = require("../index.js");
+const {io: Client} = require("socket.io-client");
+const Server = require("../webserver/Server.js");
+
+const COOKIE_NAME = "connect.sid";
+
+describe("Test Websockets", () => {
+    let clientSocket;
+    beforeAll(() => {
+        this.server = new Server();
+        this.server.start(3010);
+    });
+
+    beforeEach(async () => {
+        const res = await request(this.server.app)
+            .post('/auth/login')
+            .send({
+                username: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PWD,
+            })
+        const cookie = res.header['set-cookie'][0];
+        if (res.statusCode !== 200) {
+            throw new Error("Could not login");
+        }
+        console.log("Cookie: ", cookie);
+
+        const options = {
+            transportOptions: {
+                polling: {
+                    extraHeaders: {
+                        'Cookie': cookie
+                    }
+                }
+            },
+            reconnection: true,
+            autoConnect: true,
+            timeout: 1000,
+        }
+
+        clientSocket = Client("http://localhost:3010", options);
+    });
+
+    it("Test Settings", (done) => {
+        clientSocket.on("settingRefresh", (data) => {
+            expect('tags.tagSet.default' in data).toBe(true);
+            done();
+        })
+        clientSocket.emit("settingGetAll")
+    });
+
+    afterEach(() => {
+        clientSocket.disconnect();
+    });
+
+});
