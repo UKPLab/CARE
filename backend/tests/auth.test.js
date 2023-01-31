65d5897899256f70b1d21d0ab5610164e6c37a43
Dennis Zyska <dennis@zyska.org>
2023-01-31 15:58:29 +0100

    waiting 2 sec before starting tests

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -6,7 +6,7 @@ describe('Test Login', () => {
     beforeAll(async () => {
         this.server = new Server();
         this.server.start(3020);
-        await new Promise(resolve => setTimeout(() => resolve(), 500));
+        await new Promise(resolve => setTimeout(() => resolve(), 2000));
     });
 
     /**
