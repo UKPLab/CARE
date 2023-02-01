16ca3f61ff7d500f1f6f8f75dac33113ef0bc196
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2023-02-01 09:39:24 +0100

    try it with 5 sec

--- backend/tests/socket.test.js
+++ backend/tests/socket.test.js
@@ -10,7 +10,7 @@ describe("Test Websockets", () => {
     beforeAll(async () => {
         this.server = new Server();
         this.server.start(3010);
-        await new Promise(resolve => setTimeout(() => resolve(), 2000));
+        await new Promise(resolve => setTimeout(() => resolve(), 5000));
     });
 
     beforeEach(async () => {
