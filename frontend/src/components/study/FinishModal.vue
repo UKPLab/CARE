eaeb4d788b4dfaf6397910b84e80d050d4f4f3b1
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2023-02-08 20:12:32 +0100

    added timer and auto finish

---
+++ frontend/src/components/study/FinishModal.vue
@@ -0,0 +1,38 @@
+<template>
+  <Modal ref="modal" :props="this.$props" disable-keyboard lg name="studyFinish" remove-close>
+    <template v-slot:title>
+      Finish Study
+    </template>
+    <template v-slot:body>
+
+    </template>
+    <template v-slot:footer>
+
+    </template>
+  </Modal>
+</template>
+
+<script>
+import Modal from "@/basic/Modal.vue";
+
+export default {
+  name: "FinishModal.vue",
+  components: {Modal},
+  props: {
+    studyId: {
+      type: Number,
+      required: true,
+      default: 0,
+    }
+  },
+  methods: {
+    open() {
+      this.$refs.modal.open();
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
