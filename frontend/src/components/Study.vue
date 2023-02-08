4a8485785dacd54c8591e1c2695fbb4a475fe3c2
Dennis Zyska <dennis@zyska.org>
2023-02-08 20:12:32 +0100

    added timer and auto finish

--- frontend/src/components/Study.vue
+++ frontend/src/components/Study.vue
@@ -1,5 +1,22 @@
 <template>
-  <StudyModal v-if="studySessionId === 0" ref="studyModal" :study-id="studyId" @start="start"></StudyModal>
+  <StudyModal v-if="studySessionId === 0"
+              ref="studyModal"
+              :study-id="studyId"
+              @finish="finish"
+              @start="start"/>
+  <FinishModal ref="studyFinishModal" :study-id="studyId"/>
+  <!--
+  TODO open modal when time is up - (if not resumable add not closeable)
+  -->
+
+  <Teleport to="#topbarCustomPlaceholder">
+    <button class="btn btn-outline-secondary" type="button" @click="finish">Finish Study</button>
+    <button v-if="timeLeft > 0" class="btn mb-1" type="button">
+      <LoadIcon :size="21" class="me-1 middle" icon-name="stopwatch"/>
+      <span :class="{'text-danger':timeLeft < (5 * 60)}" class="middle"><b>Time Left:</b> {{ timeLeftHuman }}</span>
+    </button>
+
+  </Teleport>
   <Annotater v-if="documentId !== 0" :document-id="documentId"
              :readonly="readonly"
              :study-session-id="studySessionId"/>
@@ -8,13 +25,16 @@
 <script>
 import StudyModal from "./study/StudyModal.vue";
 import Annotater from "./Annotater.vue";
+import FinishModal from "./study/FinishModal.vue";
+import LoadIcon from "@/icons/LoadIcon.vue";
 
 export default {
   name: "Study",
-  components: {StudyModal, Annotater},
+  components: {LoadIcon, FinishModal, StudyModal, Annotater},
   data() {
     return {
       studySessionId: 0,
+      timeLeft: 0,
     }
   },
   props: {
@@ -76,6 +96,12 @@ export default {
       }
       return 0;
     },
+    timeLeftHuman() {
+      if (this.timeLeft < 60) {
+        return Math.round(this.timeLeft) + "s";
+      }
+      return Math.round(this.timeLeft / 60) + "min";
+    }
   },
   methods: {
     load() {
@@ -85,6 +111,24 @@ export default {
     },
     start(data) {
       this.studySessionId = data;
+      if (this.study.timeLimit > 0) {
+        this.calcTimeLeft();
+      }
+    },
+    finish(data) {
+      this.studySessionId = data;
+      this.$refs.studyFinishModal.open();
+    },
+    calcTimeLeft() {
+      const timeSinceStart = (Date.now() - new Date(this.studySession.start)) / 1000;
+      this.timeLeft = this.study.timeLimit * 60 - timeSinceStart;
+
+      if (this.timeLeft < 0) {
+        this.finish(this.studySessionId);
+      }
+      else if (!this.studySession.end) {
+        setTimeout(this.calcTimeLeft, 1000);
+      }
     }
   }
 }
