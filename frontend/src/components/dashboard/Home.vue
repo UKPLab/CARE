b5ddd15d079d032ee63b5b2f64642a617097ba5e
Dennis Zyska <zyska@ukp.informatik.tu-darmstadt.de>
2023-02-14 14:24:42 +0100

    cleaning and fixing home component

---
+++ frontend/src/components/dashboard/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <Documents />
+</template>
+
+<script>
+import Documents from "@/components/dashboard/Documents.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Documents
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
