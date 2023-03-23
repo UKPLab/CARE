88ebde68999e9dfbdfef7483bd0fc4bccfcc76e3
Nils Dycke <dycke@ukp.informatik.tu-darmstadt.de>
2023-03-23 10:28:37 +0100

    json editor started (currently only readonly mode) + pretty NLP skill view

--- frontend/src/basic/editor/JsonEditor.vue
+++ frontend/src/basic/editor/JsonEditor.vue
@@ -1,12 +1,31 @@
 <template>
   <blockquote
-    class="blockquote fs-6"
-    v-show="modelValue">
-    <div ref="content"/>
+      v-show="modelValue"
+      class="blockquote fs-6 position-relative"
+      @mouseover="showCopyButton=true"
+      @mouseleave="showCopyButton=false"
+  >
+    <div
+        ref="content"
+        class="bg-light border-start"
+    >
+      <button
+          v-show="showCopyButton"
+          class="btn position-absolute top-0 end-0 opacity-50"
+          @click="copy"
+      >
+        <LoadIcon
+            class="me-1"
+            :size="16"
+            icon-name="clipboard"
+        />
+        <span class="fw-light"> Copy </span>
+      </button>
+    </div>
   </blockquote>
   <Loader
-    v-show="!modelValue"
-    :loading="!modelValue"
+      v-show="!modelValue"
+      :loading="!modelValue"
   />
 </template>
 
@@ -14,6 +33,7 @@
 
 import Loader from "@/basic/Loader.vue";
 import jsonview from "@pgrabovets/json-view";
+import LoadIcon from "@/icons/LoadIcon.vue";
 
 /* JsonEditor.vue - default component for a JSON editor
 
@@ -25,7 +45,8 @@ Source: -
 export default {
   name: "JsonEditor",
   components: {
-    Loader
+    Loader,
+    LoadIcon
   },
   props: {
     modelValue: {
@@ -41,7 +62,8 @@ export default {
   emits: ["update:modelValue"],
   data: function () {
     return {
-      jsonTree: null
+      jsonTree: null,
+      showCopyButton: false
     }
   },
   watch: {
@@ -50,11 +72,11 @@ export default {
     }
   },
   beforeUnmount() {
-    if(this.jsonTree)
+    if (this.jsonTree)
       jsonview.destroy(this.jsonTree)
   },
   mounted() {
-   this.updateJson(this.modelValue);
+    this.updateJson(this.modelValue);
   },
   methods: {
     updateJson(data) {
@@ -67,14 +89,35 @@ export default {
       jsonview.render(newTree, this.$refs.content);
       this.jsonTree = newTree;
 
-      console.log(this.jsonTree);
-      if(this.jsonTree.children.length < 3){
-        jsonview.expand(this.jsonTree);
-        this.jsonTree.children.forEach(c => {
-          if(c.children && c.children.length > 1){
-            jsonview.collapse(c);
-          }
-          c.children.forEach(c2 => jsonview.collapse(c2));
+      jsonview.expand(this.jsonTree);
+      this.jsonTree.children.forEach(c => {
+        if (c.children && c.children.length > 1) {
+          jsonview.collapse(c);
+        }
+        c.children.forEach(c2 => jsonview.collapse(c2));
+      });
+    },
+    async copy() {
+      if (this.modelValue) {
+        try {
+          await navigator.clipboard.writeText(JSON.stringify(this.modelValue, null, 2));
+          this.eventBus.emit('toast', {
+            title: "Json copied",
+            message: "Json copied to clipboard!",
+            variant: "success"
+          });
+        } catch ($e) {
+          this.eventBus.emit('toast', {
+            title: "Json not copied",
+            message: "Could not copy json to clipboard!",
+            variant: "danger"
+          });
+        }
+      } else {
+        this.eventBus.emit('toast', {
+          title: "Json not copied",
+          message: "Json not loaded or empty, cannot copy.",
+          variant: "danger"
         });
       }
     }
@@ -84,9 +127,12 @@ export default {
 
 <style>
 blockquote div .json-container {
-  all:inherit;
+  all: inherit;
 }
+
 blockquote {
-  border-width:1px;
+  border-width: 1px;
 }
+
+
 </style>
--- frontend/src/components/dashboard/nlp_skills/SkillItem.vue
+++ frontend/src/components/dashboard/nlp_skills/SkillItem.vue
@@ -1,10 +1,50 @@
 <template>
-
+  <div class="container border border-1 rounded-3 h-100">
+    <div class="row mb-2 py-3">
+      <div class="col p-1">
+        <LoadIcon :icon-name="icon" />
+        <span class="px-1 fs-5 fw-light">{{ name }}</span>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col">
+        <JsonEditor :model-value="jsonData" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import JsonEditor from "@/basic/editor/JsonEditor.vue";
+import LoadIcon from "@/icons/LoadIcon.vue";
+
 export default {
-  name: "SkillItem"
+  name: "SkillItem",
+  components: {LoadIcon, JsonEditor},
+  props: {
+    jsonData: {
+      type: Object,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: null
+    }
+  },
+  computed: {
+    iconShown() {
+      if(this.icon){
+        return this.icon;
+      } else {
+        return Array.isArray(this.jsonData) ? "list" : "box";
+      }
+    }
+  }
 }
 </script>
 
