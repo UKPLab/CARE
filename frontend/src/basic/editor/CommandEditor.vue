dce28272f0d2ca9621ca717f32a5c138eb618d1b
Nils Dycke <n.dycke@gmx.de>
2023-03-24 11:40:15 +0100

    Updated generic JSON Editor to support editing and added command editor to the skill details modal.

---
+++ frontend/src/basic/editor/CommandEditor.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <JsonEditor v-model:content="content" start-edit-mode/>
+    <div class="justify-content-end">
+      <div class="btn-group btn-group-sm">
+        <button
+            class="btn btn-outline-secondary"
+            type="button"
+            title="Send"
+            @click="send"
+        >
+          <LoadIcon
+              icon-name="send-exclamation"
+              :size="16"
+          />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import JsonEditor from "./JsonEditor.vue";
+import LoadIcon from "@/icons/LoadIcon.vue";
+
+export default {
+  name: "CommandEditor",
+  components: {JsonEditor, LoadIcon},
+  data() {
+    return {
+      content: {}
+    }
+  },
+  methods: {
+    send(){
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
