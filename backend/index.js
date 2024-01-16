73e899ba7781e1581ebb486e14080678c52d7e36
Zheyu Zhang <oranz2049@gmail.com>
2024-01-16 22:19:21 +0100

    added functionality: save the latest document in disk

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,15 @@
  */
 const serverPort = process.env.CONTENT_SERVER_PORT || 3001;
 const Server = require("./webserver/Server");
+const DOCUMENT_PATH = `${__dirname}/../documents`;
 
 // check logging dir exists
 const fs = require("fs");
 if (!fs.existsSync(process.env.LOGGING_PATH || "./logs")) {
-    fs.mkdirSync(process.env.LOGGING_PATH || "./logs", {recursive: true});
+  fs.mkdirSync(process.env.LOGGING_PATH || "./logs", { recursive: true });
+}
+if (!fs.existsSync(DOCUMENT_PATH)) {
+  fs.mkdirSync(DOCUMENT_PATH, { recursive: true });
 }
 
 const server = new Server();
