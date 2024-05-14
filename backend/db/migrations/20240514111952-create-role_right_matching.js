e57b272edd5fbbbdfd45a1b5b9e09e3a542e2a55
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-05-14 13:28:56 +0200

    created right management tables

---
+++ backend/db/migrations/20240514111952-create-role_right_matching.js
@@ -0,0 +1,49 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('role_right_matching', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userRoleId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'user_role',
+          key: 'id'
+        },
+      },
+      roleRightKey: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        references: {
+          model: 'user_right',
+          key: 'key'
+        },
+      },
+      deleted: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        defaultValue: null,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('role_right_matching');
+  },
+};
