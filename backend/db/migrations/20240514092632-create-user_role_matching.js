a47926f9e0010dc5e9efafef220ea736cabb0141
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-05-17 14:32:54 +0200

    adjusted table columns

--- backend/db/migrations/20240514092632-create-user_role_matching.js
+++ backend/db/migrations/20240514092632-create-user_role_matching.js
@@ -7,6 +7,7 @@ module.exports = {
         allowNull: false,
         primaryKey: true,
         type: Sequelize.INTEGER,
+        autoIncrement: true,
       },
       userId: {
         allowNull: false,
