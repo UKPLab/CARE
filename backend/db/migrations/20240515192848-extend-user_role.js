a88567be38324e325ebdc7849b527dcbed8413da
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-05-25 19:58:14 +0200

    populated the user_role table with proper roles

--- backend/db/migrations/20240515192848-extend-user_role.js
+++ backend/db/migrations/20240515192848-extend-user_role.js
@@ -1,20 +1,20 @@
-'use strict';
+"use strict";
 
-const { Op } = require('sequelize');
+const { Op } = require("sequelize");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     // Fetch two rows from sysrole table where the names are regular and admin
     const selectedSysroles = await queryInterface.sequelize.query(
-      'SELECT * FROM sysrole WHERE name IN (:names)',
+      "SELECT * FROM sysrole WHERE name IN (:names)",
       {
-        replacements: { names: ['regular', 'admin'] },
+        replacements: { names: ["regular", "admin"] },
         type: queryInterface.sequelize.QueryTypes.SELECT,
       }
     );
 
     await queryInterface.bulkInsert(
-      'user_role',
+      "user_role",
       await Promise.all(
         selectedSysroles.map(async (sysrole) => {
           return {
@@ -28,11 +28,31 @@ module.exports = {
         })
       )
     );
-  },
 
+    await queryInterface.bulkInsert("user_role", [
+      {
+        name: "teacher",
+        description: "responsible for coordination",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "mentor",
+        description: "have the grading right.",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        name: "student",
+        description: "leave inline commentary",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('user_role', {
-      name: { [Op.in]: ['regular', 'admin'] },
+    await queryInterface.bulkDelete("user_role", {
+      name: { [Op.in]: ["regular", "admin", "teacher", "mentor", "student"] },
     });
   },
 };
