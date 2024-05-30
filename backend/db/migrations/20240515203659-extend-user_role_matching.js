8f2348a8f1354e4a19179a157c54ea2232f7a2a0
Linyin Huang <pdji1602003@gmail.com>
2024-05-30 12:51:09 +0200

    modified the undo function in user_role_matching migration

--- backend/db/migrations/20240515203659-extend-user_role_matching.js
+++ backend/db/migrations/20240515203659-extend-user_role_matching.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -6,14 +6,14 @@ module.exports = {
     const users = await queryInterface.sequelize.query(
       'SELECT * FROM "user" WHERE sysrole IN (:sysroles)',
       {
-        replacements: { sysroles: ['regular', 'admin'] },
+        replacements: { sysroles: ["admin"] },
         type: queryInterface.sequelize.QueryTypes.SELECT,
       }
     );
-    
+
     // Transfer the data to user_role_matching table
     for (const user of users) {
-      await queryInterface.bulkInsert('user_role_matching', [
+      await queryInterface.bulkInsert("user_role_matching", [
         {
           userId: user.id,
           userRoleName: user.sysrole,
@@ -26,32 +26,38 @@ module.exports = {
     }
 
     // Remove the sysrole column from the user table
-    await queryInterface.removeColumn('user', 'sysrole');
+    await queryInterface.removeColumn("user", "sysrole");
   },
 
   async down(queryInterface, Sequelize) {
     // Add back the sysrole column to the user table
-    await queryInterface.addColumn('user', 'sysrole', {
+    await queryInterface.addColumn("user", "sysrole", {
       type: Sequelize.STRING,
       allowNull: false,
       references: {
-        model: 'sysrole',
-        key: 'name',
+        model: "sysrole",
+        key: "name",
       },
     });
 
     // Fill in the sysrole column in the user table again
     const userRoleMatchings = await queryInterface.sequelize.query(
-      'SELECT * FROM user_role_matching',
+      "SELECT * FROM user_role_matching",
       {
         type: queryInterface.sequelize.QueryTypes.SELECT,
       }
     );
 
     for (const matching of userRoleMatchings) {
-      await User.update(
-        { sysrole: matching.userRoleName },
-        { where: { id: matching.userId } }
+      await queryInterface.sequelize.query(
+        'UPDATE "user" SET "sysrole" = :userRoleName WHERE "id" = :userId',
+        {
+          replacements: {
+            userRoleName: matching.userRoleName,
+            userId: matching.userId,
+          },
+          type: queryInterface.sequelize.QueryTypes.UPDATE,
+        }
       );
     }
   },
