c77b1d7f1585fb315180bf40d112bae38bacf119
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-05-31 17:49:39 +0200

    fixed role_right_matching migration error

--- backend/db/migrations/20240514111952-create-role_right_matching.js
+++ backend/db/migrations/20240514111952-create-role_right_matching.js
@@ -43,24 +43,8 @@ module.exports = {
         type: Sequelize.DATE,
       },
     });
-
-    await queryInterface.bulkInsert("role_right_matching", [
-      {
-        userRoleName: "teacher",
-        userRightName: "backend.socket.user.getUsers.students",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        userRoleName: "teacher",
-        userRightName: "backend.socket.user.getUsers.mentors",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("role_right_matching", null, {});
     await queryInterface.dropTable("role_right_matching");
   },
 };
