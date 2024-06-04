29ee543d5d0410630e7414cd7f4a535c61f5514f
Juliane <juliane.bechert@web.de>
2024-06-04 09:43:20 +0200

    Added comments in index.js and renamed methods

--- utils/modules/editor-delta-conversion/index.js
+++ utils/modules/editor-delta-conversion/index.js
@@ -1,12 +1,28 @@
 const Delta = require('quill-delta');
 const {QuillDeltaToHtmlConverter} = require('quill-delta-to-html');
 
-function convertToHtml(delta) {
-    const converter = new QuillDeltaToHtmlConverter(delta.ops, {}); // Does not work as intended, seems like it does not handle deletions properly
+/**
+ * Converts a Quill Delta object to HTML.
+ *
+ * This method takes a Quill Delta object as input and returns the corresponding HTML.
+ *
+ * @param {object} delta - The Quill Delta object to convert to HTML.
+ * @returns {string} The HTML representation of the Quill Delta object.
+ */
+function deltaToHtml(delta) {
+    const converter = new QuillDeltaToHtmlConverter(delta.ops, {}); 
     return converter.convert();
 } 
 
-function convert(dbEntries) {
+/**
+ * Converts an array of database entries to a Quill Delta object.
+ *
+ * This method takes an array of database entries as input and returns a Quill Delta object.
+ *
+ * @param {array} dbEntries - The array of database entries to convert to a Quill Delta object.
+ * @returns {object} The Quill Delta object representation of the database entries.
+ */
+function DbToDelta(dbEntries) {
     return dbEntries.sort((a, b) => new Date(a.createdAt) - new Date(b.createdAt)).map(edit => {
         const {operationType, offset, span, text, attributes} = edit;
 
@@ -32,7 +48,14 @@ function convert(dbEntries) {
 
 }
 
-
+/**
+ * Returns the operation type of a Quill Delta operation. Used in method deltaToDb.
+ *
+ * This method takes a Quill Delta operation as input and returns the corresponding operation type.
+ *
+ * @param {object} op - The Quill Delta operation to get the operation type for.
+ * @returns {number} The operation type of the Quill Delta operation.
+ */
 function getOperationType(op) {
     if ('insert' in op) {
         return 0; // Insert
@@ -45,6 +68,14 @@ function getOperationType(op) {
     }
 }
 
+/**
+ * Returns the span of a Quill Delta operation. Used in method deltaToDb.
+ *
+ * This method takes a Quill Delta operation as input and returns the corresponding span.
+ *
+ * @param {object} op - The Quill Delta operation to get the span for.
+ * @returns {number} The span of the Quill Delta operation.
+ */
 function getSpan(op) {
     if ('insert' in op) {
         return op.insert.length;
@@ -55,6 +86,14 @@ function getSpan(op) {
     }
 }
 
+/**
+ * Concatenates an array of Quill Delta objects into a single Quill Delta object. Used for method setContent in Editor.vue to present data in the corrct format.
+ *
+ * This method takes an array of Quill Delta objects as input and returns a single Quill Delta object.
+ *
+ * @param {array} deltas - The array of Quill Delta objects to concatenate.
+ * @returns {object} The concatenated Quill Delta object.
+ */
 function concatDeltas(deltas) {
     let result = new Delta();
   
@@ -63,9 +102,11 @@ function concatDeltas(deltas) {
           if (op.insert) {
             result.insert(op.insert, op.attributes || {});
           }
+          /*
           if (op.retain !== undefined) {
             result.retain(op.retain);
           }
+          */
           if (op.delete !== undefined) {
             result.delete(op.delete);
           }
@@ -75,6 +116,14 @@ function concatDeltas(deltas) {
     return result;
   }
 
+/**
+ * Converts a Quill Delta object to an array of database entries.
+ *
+ * This method takes a Quill Delta object as input and returns an array of database entries.
+ *
+ * @param {array} ops - The Quill Delta object to convert to database entries.
+ * @returns {array} The array of database entries.
+ */
 function deltaToDb(ops) {
     let offset = 0;
     return ops.reduce(function (pV, op) {
@@ -97,7 +146,7 @@ function deltaToDb(ops) {
 
 module.exports = {
     deltaToDb: deltaToDb,
-    convert: convert,
-    convertToHtml: convertToHtml,
+    DbToDelta: DbToDelta,
+    deltaToHtml: deltaToHtml,
     concatDeltas: concatDeltas
 }
