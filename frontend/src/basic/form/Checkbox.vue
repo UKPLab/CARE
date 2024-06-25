7121f5241753874881fe5a39dc2a7c8fdf819ab2
Linyin Huang <pdji1602003@gmail.com>
2024-06-25 10:34:22 +0200

    adapted Checkbox.vue to make it compatible with an array of checkboxes

--- frontend/src/basic/form/Checkbox.vue
+++ frontend/src/basic/form/Checkbox.vue
@@ -1,55 +1,68 @@
 <template>
   <FormElement :options="options">
     <template #element>
-      <input
-        v-model="currentData"
-        :name="options.key"
-        :required="options.required"
-        class="form-check-input"
-        type="checkbox"
+      <div
+        v-for="(option, index) in options.options"
+        :key="index"
         :class="options.class"
+        class="form-check"
       >
+        <input
+          v-model="currentData"
+          :value="option.value"
+          :name="options.key"
+          :required="options.required"
+          class="form-check-input"
+          type="checkbox"
+        />
+        <label class="form-check-label">{{ option.label }}</label>
+      </div>
     </template>
   </FormElement>
 </template>
 
 <script>
-import FormElement from "@/basic/form/Element.vue"
+import FormElement from "@/basic/form/Element.vue";
 
 export default {
   name: "FormCheckbox",
-  components: {FormElement},
+  components: { FormElement },
   props: {
     options: {
       type: Object,
       required: true,
     },
     modelValue: {
-      type: String,
+      type: Array,
       required: false,
-      default: "",
+      default: () => [],
     },
   },
   emits: ["update:modelValue"],
   data() {
     return {
-      currentData: "",
-    }
+      currentData: [],
+    };
   },
   watch: {
     currentData() {
       this.$emit("update:modelValue", this.currentData);
     },
-    modelValue() {
-      this.currentData = this.modelValue;
+    modelValue: {
+      immediate: true,
+      handler(newVal) {
+        this.currentData = newVal;
+      },
     },
   },
   mounted() {
     this.currentData = this.modelValue;
   },
-}
+};
 </script>
 
 <style scoped>
-
+.form-check {
+  margin-right: 10px;
+}
 </style>
