ddbf878ef54b12556ca661f9ff0362e598367138
Linyin Huang <pdji1602003@gmail.com>
2024-07-04 20:02:02 +0200

    reformatting admin codes

--- frontend/src/store/modules/admin.js
+++ frontend/src/store/modules/admin.js
@@ -9,90 +9,90 @@
  */
 
 const getDefaultState = () => {
-    return {
-        docs: [],
-        users: [],
-        user_stats: {}
-    };
+  return {
+    docs: [],
+    users: [],
+    user_stats: {},
+  };
 };
 
 export default {
-    namespaced: true,
-    strict: true,
-    state: getDefaultState(),
-    getters: {
-        /**
-         * Returns all users loaded in the store. May be empty or null. Requires a previous call to fetch
-         * users from the server.
-         *
-         * @param state
-         * @returns {[]}
-         */
-        getUsers: state => {
-            return state["users"]
-        },
-
-        /**
-         * Returns the stats for a given user (by id), if loaded from the server. Otherwise, null.
-         *
-         * @param state
-         * @returns {(function(Number): (Object|null))}
-         */
-        getStatsByUser: state => (userId) => {
-            if (userId in state["user_stats"]) {
-                return state["user_stats"][userId];
-            } else {
-                return null;
-            }
-        },
-
-        /**
-         * Fetch users by their role from the server
-         * 
-         * @param {*} state 
-         * @returns 
-         */
-        getUsersByRole: state => {
-            // TODO: Check if it is better to add another defaultState, e.g. userList
-            return state["users"]
-        },
+  namespaced: true,
+  strict: true,
+  state: getDefaultState(),
+  getters: {
+    /**
+     * Returns all users loaded in the store. May be empty or null. Requires a previous call to fetch
+     * users from the server.
+     *
+     * @param state
+     * @returns {[]}
+     */
+    getUsers: (state) => {
+      return state["users"];
     },
-    mutations: {
-        /**
-         * On "userData" updates the store to be set to the provided users. Replaces existing users.
-         *
-         * @param state
-         * @param message
-         */
-        SOCKET_userData: (state, message) => {
-            if (message.success) {
-                state.users = message.users;
-            }
-        },
 
-        /**
-         * On "statsDataByUser", updates the user stats associated with a given user ID.
-         *
-         * @param state
-         * @param message
-         */
-        SOCKET_statsDataByUser: (state, message) => {
-            if (message.success) {
-                state.user_stats[message.userId] = message.statistics;
-            }
-        },
-
-        /**
-         * On "respondUsersByRole", fetch the user list by their role and update the store 
-         * 
-         * @param state 
-         * @param message 
-         */
-        SOCKET_respondUsersByRole: (state, message) => {
-            if (message.success) {
-                state.users = message.users;
-            }
-        }
+    /**
+     * Returns the stats for a given user (by id), if loaded from the server. Otherwise, null.
+     *
+     * @param state
+     * @returns {(function(Number): (Object|null))}
+     */
+    getStatsByUser: (state) => (userId) => {
+      if (userId in state["user_stats"]) {
+        return state["user_stats"][userId];
+      } else {
+        return null;
+      }
     },
-    actions: {}
+
+    /**
+     * Fetch users by their role from the server
+     *
+     * @param {*} state
+     * @returns
+     */
+    getUsersByRole: (state) => {
+      // TODO: Check if it is better to add another defaultState, e.g. userList
+      return state["users"];
+    },
+  },
+  mutations: {
+    /**
+     * On "userData" updates the store to be set to the provided users. Replaces existing users.
+     *
+     * @param state
+     * @param message
+     */
+    SOCKET_userData: (state, message) => {
+      if (message.success) {
+        state.users = message.users;
+      }
+    },
+
+    /**
+     * On "statsDataByUser", updates the user stats associated with a given user ID.
+     *
+     * @param state
+     * @param message
+     */
+    SOCKET_statsDataByUser: (state, message) => {
+      if (message.success) {
+        state.user_stats[message.userId] = message.statistics;
+      }
+    },
+
+    /**
+     * On "respondUsersByRole", fetch the user list by their role and update the store
+     *
+     * @param state
+     * @param message
+     */
+    SOCKET_respondUsersByRole: (state, message) => {
+      if (message.success) {
+        state.users = message.users;
+      }
+    },
+  },
+  actions: {},
 };
