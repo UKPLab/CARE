5d3276d54cc0bb299dca8c69028f4bbd621b95ff
Linyin Huang <pdji1602003@gmail.com>
2024-07-05 11:01:27 +0200

    added associations between user and user_role_matching and added getUserDetails method

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -12,6 +12,10 @@ module.exports = (sequelize, DataTypes) => {
          */
         static associate(models) {
             // define association here
+            User.hasMany(models.models["user_role_matching"], {
+                foreignKey: 'userId',
+                as: 'roles'
+            })
         }
 
         /**
