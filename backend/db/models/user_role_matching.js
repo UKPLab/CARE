8ed1132ac44c5956b6fdd46a3c699a51b03c4b99
Linyin Huang <pdji1602003@gmail.com>
2024-07-28 17:58:07 +0200

    changed the argument passed in associate method to simplify later use

--- backend/db/models/user_role_matching.js
+++ backend/db/models/user_role_matching.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      UserRoleMatching.belongsTo(models.models["user"], {
+      UserRoleMatching.belongsTo(models["user"], {
         foreignKey: 'userId',
         as: 'user'
       })
