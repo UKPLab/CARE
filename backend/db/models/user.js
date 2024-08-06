03a0058b4b787637676a1a8c3cefcf577b2afa36
Linyin Huang <pdji1602003@gmail.com>
2024-08-06 18:36:03 +0200

    reformatted codes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,131 +1,139 @@
-'use strict';
+"use strict";
 const MetaModel = require("../MetaModel.js");
-const {Op} = require("sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
-    class User extends MetaModel {
-
-        /**
-         * Helper method for defining associations.
-         * This method is not a part of Sequelize lifecycle.
-         * The `models/index` file will call this method automatically.
-         */
-        static associate(models) {
-            // define association here
-            User.hasMany(models["user_role_matching"], {
-                foreignKey: 'userId',
-                as: 'roles'
-            })
-        }
-
-        /**
-         * Find a user by username or email
-         * @param {string} userName username or email
-         * @returns {Promise<object>} user
-         */
-        static async find(userName) {
-            try {
-                return await this.findOne({
-                    where: {
-                        [Op.or]: [{
-                            userName: userName
-                        }, {
-                            email: userName
-                        }],
-                    }, raw: true
-                });
-            } catch (err) {
-                console.log("Error in User.find: " + err);
-            }
-        }
-
-        /**
-         * Get user id by username or email
-         * @param {string} userName username or email
-         * @returns {Promise<integer>} user id
-         */
-        static async getUserIdByName(userName) {
-            const user = await User.find(userName);
-            if (user) {
-                return user.id;
-            } else {
-                return 0;
-            }
-        }
-
-        /**
-         * Get user name by id
-         * @param {number} userId user id
-         * @returns {Promise<string>} user name
-         */
-        static async getUserName(userId) {
-            try {
-                const user = await User.getById(userId);
-                if (user) {
-                    return user.userName;
-                } else {
-                    return "System";
-                }
-            } catch (e) {
-                console.log(e);
-            }
-        }
-
-        /**
-         * Get user id by email
-         * @param {string} email email
-         * @returns {Promise<integer>}} user id
-         */
-        static async getUserIdByEmail(email) {
-            const user = await User.getByKey('email', email);
-            if (user) {
-                return user.id;
-            } else {
-                return 0;
-            }
-        }
-
-        /**
-         * Register a new login
-         * @param {string} userId user id
-         * @returns {Promise<boolean>}} true if successufl
-         */
-        static async registerUserLogin(userId) {
-            try {
-                const updatedObject = await this.update({lastLoginAt: Date.now()}, {
-                        where: {
-                            id: userId
-                        },
-                        returning: true,
-                        plain: true
-                    }
-                );
-                return updatedObject !== null && updatedObject !== undefined;
-            } catch (e) {
-                console.log(e);
-            }
-        }
+  class User extends MetaModel {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models["user_role_matching"], {
+        foreignKey: "userId",
+        as: "roles",
+      });
     }
 
-    User.init({
-        firstName: DataTypes.STRING,
-        lastName: DataTypes.STRING,
-        userName: DataTypes.STRING,
-        email: DataTypes.STRING,
-        passwordHash: DataTypes.STRING,
-        acceptTerms: DataTypes.BOOLEAN,
-        acceptStats: DataTypes.BOOLEAN,
-        salt: DataTypes.STRING,
-        deleted: DataTypes.BOOLEAN,
-        lastLoginAt: DataTypes.DATE,
-        deletedAt: DataTypes.DATE,
-        createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE
-    }, {
-        sequelize,
-        modelName: 'user',
-        tableName: 'user'
-    });
+    /**
+     * Find a user by username or email
+     * @param {string} userName username or email
+     * @returns {Promise<object>} user
+     */
+    static async find(userName) {
+      try {
+        return await this.findOne({
+          where: {
+            [Op.or]: [
+              {
+                userName: userName,
+              },
+              {
+                email: userName,
+              },
+            ],
+          },
+          raw: true,
+        });
+      } catch (err) {
+        console.log("Error in User.find: " + err);
+      }
+    }
 
-    return User;
+    /**
+     * Get user id by username or email
+     * @param {string} userName username or email
+     * @returns {Promise<integer>} user id
+     */
+    static async getUserIdByName(userName) {
+      const user = await User.find(userName);
+      if (user) {
+        return user.id;
+      } else {
+        return 0;
+      }
+    }
+
+    /**
+     * Get user name by id
+     * @param {number} userId user id
+     * @returns {Promise<string>} user name
+     */
+    static async getUserName(userId) {
+      try {
+        const user = await User.getById(userId);
+        if (user) {
+          return user.userName;
+        } else {
+          return "System";
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+
+    /**
+     * Get user id by email
+     * @param {string} email email
+     * @returns {Promise<integer>}} user id
+     */
+    static async getUserIdByEmail(email) {
+      const user = await User.getByKey("email", email);
+      if (user) {
+        return user.id;
+      } else {
+        return 0;
+      }
+    }
+
+    /**
+     * Register a new login
+     * @param {string} userId user id
+     * @returns {Promise<boolean>}} true if successufl
+     */
+    static async registerUserLogin(userId) {
+      try {
+        const updatedObject = await this.update(
+          { lastLoginAt: Date.now() },
+          {
+            where: {
+              id: userId,
+            },
+            returning: true,
+            plain: true,
+          }
+        );
+        return updatedObject !== null && updatedObject !== undefined;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+
+  User.init(
+    {
+      firstName: DataTypes.STRING,
+      lastName: DataTypes.STRING,
+      userName: DataTypes.STRING,
+      email: DataTypes.STRING,
+      passwordHash: DataTypes.STRING,
+      acceptTerms: DataTypes.BOOLEAN,
+      acceptStats: DataTypes.BOOLEAN,
+      salt: DataTypes.STRING,
+      deleted: DataTypes.BOOLEAN,
+      lastLoginAt: DataTypes.DATE,
+      deletedAt: DataTypes.DATE,
+      createdAt: DataTypes.DATE,
+      updatedAt: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "user",
+      tableName: "user",
+    }
+  );
+
+  return User;
 };
