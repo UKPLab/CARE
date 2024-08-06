bd0980ccc7ff6ec3615bd18a4ae423a4f7343cef
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-08-06 11:16:51 +0200

    reformatted codes

--- backend/webserver/sockets/user.js
+++ backend/webserver/sockets/user.js
@@ -7,71 +7,70 @@ const Socket = require("../Socket.js");
  * @type {UserSocket}
  */
 module.exports = class UserSocket extends Socket {
-
-    /**
-     * Add username as creator_name of an database entry with column creator
-     *
-     * Accept data as list of objects or single object
-     * Note: returns always list of objects!
-     *
-     * @param data {object|object[]} data to update
-     * @param key {string} key of the user id field
-     * @param targetName {string} name of the target field
-     * @returns {Promise<Awaited<*&{creator_name: string|*|undefined}>[]>}
-     */
-    async updateCreatorName(data, key = 'userId', targetName = 'creator_name') {
-        if (!Array.isArray(data)) {
-            data = [data];
-        }
-
-        return Promise.all(data.map(async x => {
-            return {...x, [targetName]: await this.models['user'].getUserName(x[key])};
-        }));
+  /**
+   * Add username as creator_name of an database entry with column creator
+   *
+   * Accept data as list of objects or single object
+   * Note: returns always list of objects!
+   *
+   * @param data {object|object[]} data to update
+   * @param key {string} key of the user id field
+   * @param targetName {string} name of the target field
+   * @returns {Promise<Awaited<*&{creator_name: string|*|undefined}>[]>}
+   */
+  async updateCreatorName(data, key = "userId", targetName = "creator_name") {
+    if (!Array.isArray(data)) {
+      data = [data];
     }
 
-    /**
-     * show only specific fields of a user
-     * @param user
-     * @return {{[p: string]: any}}
-     */
-    minimalFields(user) {
-        let include = ["id", "userName"];
-        if(this.isAdmin()){
-            include.push("lastLoginAt", "sysrole", "acceptStats");
-        }
+    return Promise.all(
+      data.map(async (x) => {
+        return { ...x, [targetName]: await this.models["user"].getUserName(x[key]) };
+      })
+    );
+  }
 
-        const entries = Object.entries(user);
-        const filtered = entries.filter(([k, v]) => include.indexOf(k) !== -1);
-
-        return Object.fromEntries(filtered);
+  /**
+   * show only specific fields of a user
+   * @param user
+   * @return {{[p: string]: any}}
+   */
+  minimalFields(user) {
+    let include = ["id", "userName"];
+    if (this.isAdmin()) {
+      include.push("lastLoginAt", "sysrole", "acceptStats");
     }
 
-    /**
-     * Send all user data to the client (only for admins)
-     * @return {Promise<void>}
-     */
-    async sendUserData() {
-        if (this.isAdmin()) {
-            const users = await this.models['user'].getAll();
-            const mappedUsers = users.map(x => this.minimalFields(x));
+    const entries = Object.entries(user);
+    const filtered = entries.filter(([k, v]) => include.indexOf(k) !== -1);
 
-            this.socket.emit("userData", {success: true, users: mappedUsers});
-        } else {
-            this.socket.emit("userData", {success: false, message: "User rights and argument mismatch"});
-            this.logger.error("User right and request parameter mismatch");
-        }
+    return Object.fromEntries(filtered);
+  }
+
+  /**
+   * Send all user data to the client (only for admins)
+   * @return {Promise<void>}
+   */
+  async sendUserData() {
+    if (this.isAdmin()) {
+      const users = await this.models["user"].getAll();
+      const mappedUsers = users.map((x) => this.minimalFields(x));
+
+      this.socket.emit("userData", { success: true, users: mappedUsers });
+    } else {
+      this.socket.emit("userData", { success: false, message: "User rights and argument mismatch" });
+      this.logger.error("User right and request parameter mismatch");
     }
+  }
 
-    init() {
-
-        this.socket.on("userGetData", async (data) => {
-            try {
-                await this.sendUserData();
-            } catch (e) {
-                this.socket.emit("userData", {success: false, message: "Failed to retrieve all users"});
-                this.logger.error("DB error while loading all users from database" + JSON.stringify(e));
-            }
-        });
-
-    }
-}
+  init() {
+    this.socket.on("userGetData", async (data) => {
+      try {
+        await this.sendUserData();
+      } catch (e) {
+        this.socket.emit("userData", { success: false, message: "Failed to retrieve all users" });
+        this.logger.error("DB error while loading all users from database" + JSON.stringify(e));
+      }
+    });
+  }
+};
