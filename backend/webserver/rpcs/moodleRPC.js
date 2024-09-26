76a24c04a7802a4c6d69ea0375a6e2271401203d
Dennis Zyska <dennis@zyska.org>
2024-09-26 12:20:58 +0200

    refactor moodlerpc

--- backend/webserver/rpcs/moodleRPC.js
+++ backend/webserver/rpcs/moodleRPC.js
@@ -1,94 +1,44 @@
 const RPC = require("../RPC.js");
 const {io: io_client} = require("socket.io-client");
-const {spawn} = require("child_process");
-const path = require("path");
 
 /**
- * Hold connection and data for external RPC test service
+ * Connects to the Moodle RPC service
  *
  * @class
  * @author Alexander Bürkle, Dennis Zyska, Nils Dycke
- * @classdesc A service that connects to an external RPC service via socket.ic.
+ * @classdesc This class connects to the Moodle RPC service and provides methods to interact with it.
  * @extends RPC
  */
 module.exports = class MoodleRPC extends RPC {
-    
+
     constructor(server) {
         const url = "ws://" + process.env.RPC_MOODLE_HOST + ":" + process.env.RPC_MOODLE_PORT;
         super(server, url);
 
     }
 
-    async init() {
-        await this.reset();
-
-        // connect to test service
-        this.socket = io_client(this.url,
-            {
-                reconnection: true,
-                timeout: this.timeout,
-            }
-        );
-        this.updateEvents(this.socket);
-        this.logger.info("Connect to RPC server at " + this.url);
-        this.socket.connect();
-
-        this.logger.info("RPC initialized");
-
-        
-}
-
-async updateEvents(socket) {
-    const self = this;
-
-    // Handle connection errors
-    socket.on("connect_error", async () => {
-        setTimeout(() => {
-            if (socket) {
-                socket.connect();
-            }
-        }, self.retryDelay);
-
-    });
-
-    // Handle reconnection attempts
-    socket.on("reconnection_attempt", () => {
-        self.logger.error("RPC Test Reconnection attempt...");
-    });
-
-    // establishing a connection
-    socket.on("connect", function () {
-        self.logger.info(`Connection to RPC server established: ${socket.connected}`);
-
-    });
-
-    // deal with broken connection
-    socket.on("disconnect", function () {
-        self.logger.error(`Connection to RPC server disrupted: ${!socket.connected}`);
-    });
-}
-
     /**
-     * This method should be overwritten to handle the call to the RPC service
-     * @param data
-     * @returns {Promise<*>}
+     * Send request to moodle RPC service
+     *
+     * @param {Object} data - The data object
+     * @param {String} eventName - The request name to send to the RPC service
+     * @returns {Promise<Object>} - The response from the RPC service
+     * @throws {Error} If the RPC service call fails or returns an unsuccessful response.
      */
-    async call(data) {
-        this.logger.info("Calling RPC service...");
+    async request(eventName, data) {
+        this.logger.info("Calling RPC service with request: " + eventName);
 
-        try {
-            this.logger.info(data);
-            return this.emit("call", data);
-            
-        } catch (err) {
-            throw err
+        const response = this.emit(eventName, data);
+        if (!response['success']) {
+            this.logger.error("Error in request " + eventName + ": " + response['message']);
+            throw new Error(response['message']);
         }
-
+        return response;
     }
 
 
     /**
-     * Retrieves users from a specified moodle course and returns the data in csv format.
+     * Retrieves users from a specified moodle course and returns the data as an object.
      *
      * @param {Object} data - The data object containing the course ID, Moodle URL and the API token.
      * @param {number} data.courseID - The ID of the course to fetch users from.
@@ -98,18 +48,10 @@ async updateEvents(socket) {
      * @throws {Error} If the RPC service call fails or returns an unsuccessful response.
      */
     async getUsersFromCourse(data) {
-        this.logger.info("Calling RPC service...");
-
         try {
-            this.logger.info(data);
-            const response = this.emit("getUsersFromCourse", data);
-            if (response.success === false) {
-                throw new Error(response.message);
-            }
-            return response;
-            
+            return this.request("getUsersFromCourse", data);
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 
@@ -125,18 +67,10 @@ async updateEvents(socket) {
      * @throws {Error} If the RPC service call fails or returns an unsuccessful response.
      */
     async getUsersFromAssignment(data) {
-        this.logger.info("Calling RPC service...");
-
         try {
-            this.logger.info(data);
-            const response = this.emit("getUsersFromAssignment", data);
-            if (response.success === false) {
-                throw new Error(response.message);
-            }
-            return response;
-            
+            return this.request("getUsersFromAssignment", data);
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 
@@ -149,25 +83,17 @@ async updateEvents(socket) {
      * @param {string} data.options.apiKey - The API token for the Moodle instance
      * @param {string} data.options.url - The URL of the Moodle instance.
      * @returns {Promise<List>} - A list of dictionaries, each containing:
-            - 'userid' (int): The ID of the user who made the submission.
-            - 'submissionURLs' (list): A list of dictionaries, each containing:
-                - 'filename' (str): The name of the submitted file.
-                - 'fileurl' (str): The URL to access the submitted file.
+     - 'userid' (int): The ID of the user who made the submission.
+     - 'submissionURLs' (list): A list of dictionaries, each containing:
+     - 'filename' (str): The name of the submitted file.
+     - 'fileurl' (str): The URL to access the submitted file.
      * @throws {Error} If the RPC service call fails or returns an unsuccessful response.
      */
     async getSubmissionInfosFromAssignment(data) {
-        this.logger.info("Calling RPC service...");
-
         try {
-            this.logger.info(data);
-            const response = this.emit("getSubmissionInfosFromAssignment", data);
-            if (response.success === false) {
-                throw new Error(response.message);
-            }
-            return response;
-            
+            return this.request("getSubmissionInfosFromAssignment", data);
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 
@@ -179,18 +105,10 @@ async updateEvents(socket) {
      * @throws {Error} If the RPC service returns a failure response or an error occurs during the process.
      */
     async downloadSubmissionsFromUser(data) {
-        this.logger.info("Calling RPC service...");
-
         try {
-            this.logger.info(data);
-            const response = this.emit("downloadSubmissionsFromUser", data);
-            if (response.success === false) {
-                throw new Error(response.message);
-            }
-            return response;
-            
+            return this.request("downloadSubmissionsFromUser", data);
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 
@@ -206,18 +124,10 @@ async updateEvents(socket) {
      * @returns {Promise<void>} - A promise that resolves when the passwords have been uploaded.
      */
     async uploadPasswordsToMoodle(data) {
-        this.logger.info("Calling RPC service...");
-
         try {
-            this.logger.info(data);
-            const response = this.emit("uploadPasswordsToMoodle", data);
-            if (response.success === false) {
-                throw new Error(response.message);
-            }
-            return response;
-            
+            return this.request("uploadPasswordsToMoodle", data);
         } catch (err) {
-            throw err
+            throw err;
         }
     }
 
