523a9be90875d4d6eadea134c81a57f4bd38f27d
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-10-02 11:38:53 +0200

    refactored codes and removed unused files

--- backend/webserver/Server.js
+++ backend/webserver/Server.js
@@ -73,7 +73,6 @@ module.exports = class Server {
         this.logger.debug("Initializing Routes for config...");
         require("./routes/config")(this);
         require('./routes/auth')(this);
-        require('./routes/download')(this);
 
         // all further urls reference to frontend
         this.app.use("/*", express.static(`${__dirname}/../../dist/index.html`));
