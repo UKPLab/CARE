16878d6fd0e8d6d17c3aa4382638910c63e1b27e
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-11-04 22:23:09 +0100

    expanded user_rights and role_rights table

---
+++ backend/db/migrations/20241031110418-basic-user_rights.js
@@ -0,0 +1,40 @@
+"use strict";
+
+const userRights = [
+  {
+    name: "frontend.dashboard.studies.addBulkAssignments",
+    description: "access to the functionality of bulk adding assignments on the dashboard",
+  },
+  {
+    name: "frontend.dashboard.studies.view",
+    description: "access to view open studies and sessions",
+  },
+  {
+    name: "frontend.dashboard.studies.view.readOnly",
+    description: "access to open sessions in a read-only mode",
+  },
+  {
+    name: "frontend.dashboard.studies.view.userInfo",
+    description: "access to view user's information",
+  },
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "user_right",
+      userRights.map((right) => {
+        right["createdAt"] = new Date();
+        right["updatedAt"] = new Date();
+        return right;
+      }),
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("user_right", {
+        name: userRights.map((r) => r.name),
+      }, {});
+  },
+};
