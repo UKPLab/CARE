31e130781d427e70723d09e25c605cc1e55ef776
linyin huang <lin-yin.huang@stud.tu-darmstadt.de>
2024-11-22 14:35:10 +0100

    revised codes according to mr comment

--- backend/index.js
+++ backend/index.js
@@ -5,11 +5,18 @@
  */
 const serverPort = process.env.CONTENT_SERVER_PORT || 3001;
 const Server = require("./webserver/Server");
+const path = require("path");
+const UPLOAD_PATH = path.join(__dirname, "..", "files");
 
 // check logging dir exists
 const fs = require("fs");
 if (!fs.existsSync(process.env.LOGGING_PATH || "./logs")) {
   fs.mkdirSync(process.env.LOGGING_PATH || "./logs", { recursive: true });
+}
+
+// check upload dir exists
+if (!fs.existsSync(UPLOAD_PATH)) {
+  fs.mkdirSync(UPLOAD_PATH, { recursive: true });
 }
 
 const server = new Server();
