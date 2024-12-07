e9abc471320daeddd10f2a01baca2ef4a7c7c5c4
Dennis Zyska <dennis@zyska.org>
2024-12-07 15:33:32 +0100

    cleanup

--- backend/webserver/sockets/assignment.js
+++ backend/webserver/sockets/assignment.js
@@ -5,136 +5,10 @@ const _ = require("lodash");
 /**
  * Handle user through websocket
  *
- * @author Alexander Bürkle
+ * @author Dennis Zyska, Alexander Bürkle
  * @type {AssignmentSocket}
  */
 module.exports = class AssignmentSocket extends Socket {
-    /**
-     * Retrieves assignment information for users and emits the data through a socket.
-     *
-     * This function fetches data from multiple models including documents, user-role matching, users, and user roles.
-     * It then processes this data to create a comprehensive list of user information, including their documents,
-     * number of assignments, and role names. The resulting data is emitted through a socket event.
-     *
-     * @async
-     * @function getAssignmentInfosFromUser
-     * @returns {Promise<void>} Emits the user information through a socket event.
-     * @throws Will emit an error message through the socket if any error occurs during data retrieval or processing.
-     */
-    async getAssignmentInfosFromUser() {
-        try {
-            if (await this.isAdmin()) {
-                const [documentUserIds, matchedUsers, users, roleIdMap] = await Promise.all([this.models["document"].findAll({
-                    where: {readyForReview: true}, attributes: ["userId", "id", "name"], raw: true,
-                }), this.models["user_role_matching"].findAll({
-                    attributes: ["userId", "userRoleId"], raw: true,
-                }), this.models["user"].findAll({
-                    attributes: {exclude: ["passwordHash", "salt"]}, raw: true,
-                }), this.models["user_role"].findAll({
-                    attributes: ["id", "name"], raw: true,
-                })]);
-
-                const idToNameMap = new Map(roleIdMap.map(({id, name}) => [id, name]));
-                const matchedUserMap = new Map(matchedUsers.map(({userId, userRoleId}) => [userId, userRoleId]));
-
-                const userInfos = users.map(user => {
-                    const documents = documentUserIds.filter(doc => doc.userId === user.id);
-                    const roleName = idToNameMap.get(matchedUserMap.get(user.id)) || null;
-
-                    return {
-                        ...user, documents, numberAssignments: documents.length, role: roleName,
-                    };
-                });
-
-                this.socket.emit("assignmentUserInfos", {
-                    success: true, userInfos,
-                });
-
-            } else {
-                throw new Error("You are not authorized to access this information. Please log in as an admin.");
-            }
-        } catch (error) {
-            this.logger.error(error);
-            this.socket.emit("assignmentUserInfos", {
-                success: false, message: error.message
-            });
-        }
-    }
-
-
-    /**
-     * Assigns peer reviews to assignments based on the specified number of reviewers per role.
-     *
-     * @param {Array<Object>} assignments - The list of assignments to be reviewed. Each assignment should have a `userId` and `documentName`.
-     * @param {Array<Object>} reviewers - The list of reviewers available for assignments. Each reviewer should have an `id` and `role`.
-     * @param {Object} reviewsPerRole - An object specifying the number of reviewers needed per role. Keys are role names and values are the number of reviewers.
-     * @param {Object} template - A template object containing properties for creating study sessions.
-     *
-     * @returns {Array<Object>} - The list of assignments with their assigned reviewers grouped by role.
-     */
-    async assignPeerReviews(data, options) {
-        const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-        const assignmentTracker = new Map();
-
-        data.assignments.forEach((assignment) => {
-            const uniqueAssignmentKey = `${assignment.userId}_${assignment.documentName}`;
-            assignmentTracker.set(uniqueAssignmentKey, new Map(Object.keys(data.reviewsPerRole).map(role => [role, []])));
-        });
-
-        const reviewersByRole = data.reviewers.reduce((acc, reviewer) => {
-            const role = reviewer.role;
-            if (!acc[role]) acc[role] = [];
-            acc[role].push(reviewer);
-            return acc;
-        }, {});
-
-        data.assignments.forEach((assignment) => {
-            const uniqueAssignmentKey = `${assignment.userId}_${assignment.documentName}`;
-            for (const [role, numReviewersStr] of Object.entries(data.reviewsPerRole)) {
-                const numReviewersNeeded = parseInt(numReviewersStr, 10);
-
-                if (numReviewersNeeded === 0) continue;
-
-                const availableReviewers = reviewersByRole[role] || [];
-                const shuffledReviewers = shuffleArray(availableReviewers);
-                let assignedReviewersCount = assignmentTracker.get(uniqueAssignmentKey).get(role).length;
-
-                for (const reviewer of shuffledReviewers) {
-                    if (assignment.userId !== reviewer.id && !assignmentTracker.get(uniqueAssignmentKey).get(role).includes(reviewer.id)) {
-                        if (assignedReviewersCount < numReviewersNeeded) {
-                            assignmentTracker.get(uniqueAssignmentKey).get(role).push(reviewer.id);
-                            assignedReviewersCount++;
-                        }
-                    }
-                    if (assignedReviewersCount >= numReviewersNeeded) break;
-                }
-            }
-        });
-        const result = data.assignments.map((assignment) => {
-            const uniqueAssignmentKey = `${assignment.userId}_${assignment.documentName}`;
-            const assignedReviewers = [];
-
-            for (const [role, reviewers] of assignmentTracker.get(uniqueAssignmentKey)) {
-                assignedReviewers.push(...reviewers);
-            }
-
-            return {
-                ...assignment, assignedReviewers,
-            };
-        });
-
-        const template = data.template
-
-        for (const assignment of result) {
-            await this.createAssignment({
-                assignment,
-                reviewers: assignment.assignedReviewers,
-                template,
-                documents: [assignment.documentName]
-            }, options);
-        }
-        return result;
-    }
 
     /**
      * Assigns a peer review task to a list of reviewers based on a given template.
@@ -455,50 +329,11 @@ module.exports = class AssignmentSocket extends Socket {
 
     }
 
-    /**
-     * Assigns reviewers (Hiwis) to assignments based on the provided data.
-     *
-     * @param {Object} data - The data for assigning reviewers.
-     * @param {Object} data.reviewers - An object where keys are reviewer IDs and values are the number of assignments they should review.
-     * @param {Array} data.assignments - An array of assignment objects to be reviewed.
-     * @param {string} data.template - The template to be used for the peer review.
-     * @param {string} data.createdByUserId - The ID of the user who created the assignments.
-     * @returns {Promise<void>} A promise that resolves when all assignments have been assigned reviewers.
-     */
-    async assignHiwis(data) {
-        let reviewersPool = [];
-        for (const [reviewerId, count] of Object.entries(data.reviewers)) {
-            const numericCount = Number(count); // Convert count to a number
-            if (numericCount > 0) {
-                reviewersPool = reviewersPool.concat(Array(numericCount).fill(Number(reviewerId)));
-            }
-        }
-        const shuffleArray = (array) => array.sort(() => Math.random() - 0.5);
-        reviewersPool = shuffleArray(reviewersPool);
-
-
-        const assignmentsWithReviewers = data.assignments.map((assignment, index) => {
-            const reviewer = reviewersPool[index % reviewersPool.length];
-            return {...assignment, reviewer}; // Assign a reviewer to the assignment
-        });
-
-        await Promise.all(assignmentsWithReviewers.map(assignment => this.assignPeerReview(assignment, Array.of(assignment.reviewer), data.template, data.createdByUserId)));
-    }
-
     init() {
 
         this.createSocket("assignmentCreate", this.createAssignment, {}, true);
         this.createSocket("assignmentCreateBulk", this.createAssignmentBulk, {}, true);
         this.createSocket("assignmentAdd", this.addReviewer, {}, true);
-
-
-        this.createSocket("assignmentGetAssignmentInfosFromUser", this.getAssignmentInfosFromUser, {}, true);
-
-        this.createSocket("assignmentAssignHiwis", this.assignHiwis, {}, true);
-
-        this.createSocket("assignmentAssignPeerReviews", this.assignPeerReviews, {}, true);
-
-
 
     }
 };
