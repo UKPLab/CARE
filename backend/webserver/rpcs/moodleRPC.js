1864a318cdc6b012c476af72791be71250a223fc
Linyin Huang <pdji1602003@gmail.com>
2025-01-17 15:58:26 +0100

    created relevant methods for retrieving assignment info

--- backend/webserver/rpcs/moodleRPC.js
+++ backend/webserver/rpcs/moodleRPC.js
@@ -57,7 +57,7 @@ module.exports = class MoodleRPC extends RPC {
     }
 
     /**
-     * Retrieves users from a specified assignemtn in a moodle course and returns the data as an array.
+     * Retrieves users from a specified assignment in a moodle course and returns the data as an array.
      *
      * WARNING: This method only works, if at least one submission has been made to the assignment.
      * If you need to use it before any students have submitted, you can submit a dummy file to the assignment.
@@ -73,6 +73,25 @@ module.exports = class MoodleRPC extends RPC {
     async getUsersFromAssignment(data) {
         try {
             return this.request("getUsersFromAssignment", data);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * Retrieves information about all assignments in a specified Moodle course.
+     *
+     * @param {Object} data - The data object containing the course ID, Moodle URL and the API token.
+     * @param {number} data.options.courseID - The ID of the course to fetch assignments from.
+     * @param {string} data.options.apiKey - The API token for the Moodle instance
+     * @param {string} data.options.url - The URL of the Moodle instance.
+     * TODO: the return data structure to be decided
+     * @returns {Promise<List>} - Information about the assignments in the course
+     * @throws {Error} If the RPC service call fails or returns an unsuccessful response.
+     */
+    async getAssignmentInfoFromCourse(data) {
+        try {
+            return this.request("getAssignmentInfoFromCourse", data);
         } catch (err) {
             throw err;
         }
@@ -130,6 +149,4 @@ module.exports = class MoodleRPC extends RPC {
     async publishAssignmentTextFeedback(data) {
         return await this.request("publishAssignmentTextFeedback", data);
     }
-
-
 }
