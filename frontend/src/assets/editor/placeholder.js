35b6db85319a608b5dd93a9c68444321dbbf7a2e
Manu Sundar Raj Nandyal <manusnandyal2001@gmail.com>
2025-02-06 23:09:35 +0100

    Change extractPlaceholder method

--- frontend/src/assets/editor/placeholder.js
+++ frontend/src/assets/editor/placeholder.js
@@ -1,42 +1,34 @@
 import Quill from "quill";
 
 /**
- * Extract placeholders from a document.
- * @param {*} document 
- * @param {*} regex 
+ * Extract placeholders from given text.
+ * @param {*} docText text which needs to be parsed
+ * @param {*} pattern pattern to be matched
  * @returns results
  */
-export function extractPlaceholder(documentText, regex) {
-    const { deltas } = documentText || {};
-    const results = [];
+export function extractPlaceholder(docText, pattern) {
+    const results = [];    
+    let match;
 
-    if (deltas) {
-        let quill = new Quill(document.createElement('div'));
-        quill.setContents(deltas);
-        const content = quill.getText();    
-        
-        let match;
-    
-        while ((match = regex.exec(content)) !== null) {
+    while ((match = pattern.exec(docText)) !== null) {
         const index = match.index;
         const text = match[0];
-    
+
         // Get all text before the placeholder
-        const fullBeforeText = content.slice(0, index).trim();
+        const fullBeforeText = docText.slice(0, index).trim();
         const previewBefore = fullBeforeText.length > 100
             ? fullBeforeText.slice(0, 100) + "..."
             : fullBeforeText;
-    
+
         // Get all text after the placeholder
-        const fullAfterText = content.slice(index + text.length).trim();
+        const fullAfterText = docText.slice(index + text.length).trim();
         const previewAfter = fullAfterText.length > 100
             ? fullAfterText.slice(0, 100) + "..."
             : fullAfterText;
-    
+
         results.push({ text, previewBefore, previewAfter });
-        }
     }
-  
+
     return results;
   }
   
