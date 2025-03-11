97c617c889c7502fc242f6c59137e7378490215e
Manu Sundar Raj Nandyal <manusnandyal2001@gmail.com>
2025-03-11 08:41:25 +0100

    Fix issues related to maximum recursive updates and destroyal of charts errors

--- frontend/src/components/stepmodal/Chart.vue
+++ frontend/src/components/stepmodal/Chart.vue
@@ -1,73 +1,100 @@
-  <template>
-    <canvas ref="chartCanvas" :id="chartType+chartOptions"></canvas>
-  </template>
-  
-  <script>
-  import { Chart, registerables } from 'chart.js';
-  
-  /**
+<template>
+  <canvas ref="chartCanvas" :id="chartType + JSON.stringify(chartOptions)"></canvas>
+</template>
+
+<script>
+import { Chart, registerables } from 'chart.js';
+import { debounce } from 'lodash';
+
+/**
  * Component to render a chart based on the provided data and options.
  * This is mainly based on chart.js component. Flexible graphs can be built according to the data.
  *
  * @author: Manu Sundar Raj Nandyal
  */
-  export default {
-    name: 'ChartComponent',
-    props: {
-      chartType: {
-        type: String,
-        required: true,
-        validator: (value) => {
-          return [
-            'bar', 'line', 'pie', 'doughnut', 'radar', 'polarArea', 'bubble', 'scatter',
-          ].includes(value);
-        },
-      },
-      chartData: {
-        type: Object,
-        required: true,
-      },
-      chartOptions: {
-        type: Object,
-        default: () => ({}),
+export default {
+  name: 'ChartComponent',
+  props: {
+    chartType: {
+      type: String,
+      required: true,
+      validator: (value) => {
+        return [
+          'bar', 'line', 'pie', 'doughnut', 'radar', 'polarArea', 'bubble', 'scatter',
+        ].includes(value);
       },
     },
-    mounted() {
-      Chart.register(...registerables);
-      this.renderChart();
+    chartData: {
+      type: Object,
+      required: true,
     },
-    beforeUnmount() {
-      this.$refs.chartCanvas.remove();
+    chartOptions: {
+      type: Object,
+      default: () => ({}),
     },
-    methods: {
-      renderChart() {
-        const ctx = this.$refs.chartCanvas.getContext('2d');
-        new Chart(ctx, {
-          type: this.chartType,
-          data: this.chartData,
-          options: this.chartOptions,
-        });
-      },
+  },
+  data() {
+    return {
+      chartInstance: null,
+      previousChartData: null,
+      previousChartType: null,
+      previousChartOptions: null,
+    };
+  },
+  mounted() {
+    Chart.register(...registerables);
+    this.renderChart();
+  },
+  beforeUnmount() {
+    this.destroyChart();
+  },
+  methods: {
+    renderChart: debounce(function () {
+      if (
+        this.previousChartData === this.chartData &&
+        this.previousChartType === this.chartType &&
+        this.previousChartOptions === this.chartOptions
+      ) {
+        return;
+      }
+
+      this.previousChartData = this.chartData;
+      this.previousChartType = this.chartType;
+      this.previousChartOptions = this.chartOptions;
+
+      this.destroyChart();
+      const ctx = this.$refs.chartCanvas.getContext('2d');
+      this.chartInstance = new Chart(ctx, {
+        type: this.chartType,
+        data: this.chartData,
+        options: this.chartOptions,
+      });
+    }, 100), 
+    destroyChart() {
+      if (this.chartInstance) {
+        this.chartInstance.destroy();
+      }
     },
-    watch: {
-      chartData: {
-        handler: 'renderChart',
-        deep: true,
-      },
-      chartType: {
-        handler: 'renderChart',
-      },
-      chartOptions: {
-        handler: 'renderChart',
-        deep: true,
-      },
+  },
+  watch: {
+    chartData: {
+      handler: 'renderChart',
+      deep: true,
     },
-  };
-  </script>
-  
-  <style scoped>
-  canvas {
-    max-width: 100%;
-    height: auto;
-  }
-  </style>
+    chartType: {
+      handler: 'renderChart',
+    },
+    chartOptions: {
+      handler: 'renderChart',
+      deep: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+canvas {
+  max-width: 100%;
+  height: auto;
+}
+</style>
