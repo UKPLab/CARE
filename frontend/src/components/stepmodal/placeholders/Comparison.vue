cccb25b2e40a1ac0db7bf0c8bdbc096495016310
Manu Sundar Raj Nandyal <manusnandyal2001@gmail.com>
2025-05-04 13:30:55 +0200

    Render the documentText and placeholders as segments in the StepModal

--- frontend/src/components/stepmodal/placeholders/Comparison.vue
+++ frontend/src/components/stepmodal/placeholders/Comparison.vue
@@ -1,43 +1,79 @@
 <template>
-    <div class="comparison-container">
-      <Chart :chartInput="combinedInput" />
-    </div>
-  </template>
-  
-  <script>
-  import Chart from "./Chart.vue";
-  
-  export default {
-    name: "Comparison",
-    components: { Chart },
-    props: {
-      title: {
-        type: String,
-        default: "Comparison Chart"
-      },
-      datasetA: {
-        type: Object,
-        required: true,
-      },
-      datasetB: {
-        type: Object,
-        required: true,
-      },
+  <div class="comparison-container">
+    <Chart v-if="combinedInput" :chartInput="combinedInput" />
+    <p v-else class="text-muted"> ~ Placeholder data missing or invalid ~ </p>
+  </div>
+</template>
+
+<script>
+/**
+ * Component to render a comparison chart based on the provided input data.
+ * This component is designed to compare two datasets on horizontal stacked bar chart.
+ * 
+ * @author: Manu Sundar Raj Nandyal
+*/
+import Chart from "./Chart.vue";
+
+export default {
+  name: "Comparison",
+  components: { Chart },
+  props: {
+    input: {
+      type: Array,
+      required: true,
     },
-    computed: {
-      combinedInput() {
-        return {
-          title: this.title,
-          datasets: [this.datasetA, this.datasetB]
-        };
+    studySteps: {
+      type: Array,
+      required: true,
+    },
+    studyData: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    combinedInput() {
+      if (this.input.length !== 2) {
+        return null;
       }
-    }
-  };
-  </script>
-  
-  <style scoped>
-  .comparison-container {
-    padding: 1rem;
-  }
-  </style>
-  
+
+      const datasets = this.input.map((value, index) => {
+        return {
+          label: `Dataset ${index + 1}`,
+          data: Object.values(value),
+          backgroundColor: `rgba(${index === 0 ? '255, 99, 132' : '54, 162, 235'}, 0.5)`
+        };
+      });
+
+      
+      return {
+        type: 'bar',
+        data: {
+          labels: Object.keys(this.input[0]),
+          datasets,
+        },
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Comparison Chart',
+            },
+            indexAxis: "y",
+          },
+        },
+      };
+      
+    },
+  },
+};
+</script>
+
+<style scoped>
+.comparison-container {
+  padding: 1rem;
+}
+</style>
