0b2282f4be41b213025205da63c75e3513effd61
Linyin Huang <pdji1602003@gmail.com>
2025-10-08 09:50:28 +0200

    refactor: remove configuration retrieving socket event

--- backend/webserver/sockets/configuration.js
+++ backend/webserver/sockets/configuration.js
@@ -10,49 +10,6 @@ const Socket = require("../Socket");
  */
 class ConfigurationSocket extends Socket {
   /**
-   * Get configuration files filtered by type
-   *
-   * @param {Object} data The data object containing the configuration type filter
-   * @param {number} data.type The type of configuration to retrieve (e.g., 0='assessment', 1='validation')
-   * @param {Object} options Additional configuration parameters
-   * @returns {Promise<Object[]>} A list of configuration files
-   */
-  async getConfigurations(data, options) {
-    const { type } = data;
-
-    if (typeof type === "undefined" || type === null) {
-      throw new Error("Configuration type is required");
-    }
-
-    if (typeof type !== "number") {
-      throw new Error("Unknown configuration type");
-    }
-
-    // Apply access filters
-    const baseWhere = { deleted: false, type };
-    const { filter, attributes, accessAllowed } = await this.getFiltersAndAttributes(
-      this.userId,
-      baseWhere,
-      { exclude: ["deleted", "deletedAt"] },
-      "configuration"
-    );
-    if (!accessAllowed) {
-      throw new Error("Access denied");
-    }
-
-    const rows = await this.models["configuration"].getAll({ where: filter, attributes });
-
-    const configs = rows.map((cfg) => ({
-      id: cfg.id,
-      name: cfg.name,
-      userId: cfg.userId,
-      config: cfg.content,
-    }));
-
-    return configs;
-  }
-
-  /**
    * Update configuration content
    *
    * @param {Object} data The data object containing the configuration update
@@ -128,7 +85,6 @@ class ConfigurationSocket extends Socket {
   }
 
   init() {
-    this.createSocket("configurationGetByType", this.getConfigurations, {}, false);
     this.createSocket("configurationUpdate", this.updateConfiguration, {}, true);
     this.createSocket("configurationAdd", this.createConfiguration, {}, true);
   }
