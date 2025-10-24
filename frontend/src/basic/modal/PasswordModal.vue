fa68d7840de4610a312086609a17f4652b8af9db
Jannik Holmer <bingobongomann@gmail.com>
2025-10-24 13:59:05 +0200

    fix: password reset modal does no longer require the old password for setting a new one if the user is an admin, unless the admins password is changed then it still requires the old password.

--- frontend/src/basic/modal/PasswordModal.vue
+++ frontend/src/basic/modal/PasswordModal.vue
@@ -36,7 +36,7 @@ import BasicButton from "@/basic/Button.vue";
 /**
  * Modal for resetting user's password
  *
- * @author: Linyin Huang
+ * @author: Linyin Huang, Jannik Holmer
  */
 export default {
   name: "PasswordModal",
@@ -45,7 +45,7 @@ export default {
     return {
       userId: 0,
       data: {},
-      fields: [
+      allFields: [
         {
           key:"oldPassword",
           type: "password",
@@ -73,6 +73,19 @@ export default {
         }
       ],
     };
+  },
+  computed: {
+    isAdmin() {
+      return this.$store.getters['auth/isAdmin'];
+    },
+    fields() {
+      const thisUserID = this.$store.getters['auth/getUserId'];
+      if (this.isAdmin && !(this.userId === thisUserID)) {
+        return this.allFields.slice(1);
+      } else {
+        return this.allFields;
+      }
+    }
   },
   methods: {
     open(id) {
