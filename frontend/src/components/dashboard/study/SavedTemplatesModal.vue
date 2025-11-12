dc219823905d8509614fc059635f2a94b6d2c534
Karim Ouf <karimouf00@gmail.com>
2025-11-12 14:11:55 +0100

    refactor: add right to delete template only for admin

--- frontend/src/components/dashboard/study/SavedTemplatesModal.vue
+++ frontend/src/components/dashboard/study/SavedTemplatesModal.vue
@@ -104,6 +104,9 @@ export default {
             }
           },
           title: "Delete",
+          filter: [
+            {key: "showDeleteTemplateButton", value: true},
+          ],
           action: "deleteTemplate",
         },
         {
@@ -122,7 +125,20 @@ export default {
   },
   computed:{
     savedTemplates() {
-      return this.$store.getters["table/study/getFiltered"]((s) => s.template === true);
+      return this.$store.getters["table/study/getFiltered"]((s) => s.template === true).map((s) => {
+        return {
+          id: s.id,
+          name: s.name,
+          createdAt: new Date(s.createdAt).toLocaleDateString(),
+          resumable: s.resumable,
+          collab: s.collaborative,
+          multipleSubmit: s.multipleSubmissions,
+          showDeleteTemplateButton: this.showDeleteTemplateButton,
+        };
+      });
+    },
+    showDeleteTemplateButton() {
+      return this.$store.getters["auth/checkRight"]("study.template.delete");
     }
   },
   methods: {
