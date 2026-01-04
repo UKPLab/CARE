e7eca6731f8796f4cfbba2264d5bd5d00cea08b8
Mohammad Elwan <mohammadsherifl11@gmail.com>
2026-01-04 19:40:36 +0100

    feat : add privacy warning for non-anonymized studies with templates

--- frontend/src/basic/dashboard/Coordinator.vue
+++ frontend/src/basic/dashboard/Coordinator.vue
@@ -21,6 +21,7 @@
         </slot>
       </span>
       <span v-else>
+        <slot name="before-form" :data="data" />
         <BasicForm
           ref="form"
           v-model="data"
@@ -89,6 +90,7 @@ import { sorter } from "@/assets/utils.js";
  * @slot success: Use slot to overwrite success message
  * @slot success-footer: Use slot to overwrite footer for if success
  * @slot footer: Use slot to overwrite footer for the modal
+ * @slot before-form: Use slot to add custom content before the form (receives form data as prop)
  * @emits submit: Submit event with the new data content
  *
  * @author: Dennis Zyska, Linyin Huang
