2e04fc69ae235d1a9917665b385935b0a0a029ce
Mohammad Elwan <mohammadsherifl11@gmail.com>
2026-01-18 21:44:28 +0100

    feat : add placeholderDescription to template_placeholder_mapping

--- backend/db/models/template_placeholder_mapping.js
+++ backend/db/models/template_placeholder_mapping.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
       templateType: DataTypes.INTEGER,
       placeholderKey: DataTypes.STRING,
       placeholderLabel: DataTypes.STRING,
+      placeholderDescription: DataTypes.TEXT,
       placeholderType: DataTypes.STRING,
       required: DataTypes.BOOLEAN,
       deleted: DataTypes.BOOLEAN,
