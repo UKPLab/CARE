95cc5374770dfcdebb07141cf329bcd81d10b9e9
Jannik Holmer <bingobongomann@gmail.com>
2026-04-20 16:03:40 +0200

    Added console logs to getVersion

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -17,12 +17,17 @@ import { execSync } from 'child_process';
 const getVersion = () => {
   let ciHash = process.env.TAG_COMMIT;
   let tag_latest = process.env.TAG_LATEST;
-  if (tag_latest && ciHash) return tag_latest + ": " + ciHash;
+  if (tag_latest && ciHash){
+    console.log("inside getVersion first if ");
+    return tag_latest + ": " + ciHash;
+  }
   try {
     ciHash = execSync('git rev-parse --short HEAD').toString().trim();
     tag_latest = execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
+    console.log("inside getVersion try");
     return tag_latest + ": " + ciHash;
   } catch (e) {
+    console.log("inside getVersion error");
     console.log(e.toString());
     // Fallback if no git is found
     return 'dev-build';
