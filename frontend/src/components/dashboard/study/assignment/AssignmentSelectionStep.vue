5fed548eb6065283c681bd4fd644f138b4a0d3d2
romila25 <romila.singh25@gmail.com>
2026-09-25 14:42:44 +0200

    fix: remove the group id column

--- frontend/src/components/dashboard/study/assignment/AssignmentSelectionStep.vue
+++ frontend/src/components/dashboard/study/assignment/AssignmentSelectionStep.vue
@@ -128,7 +128,6 @@ export default {
         { name: this.$t("common.userName"), key: "userName" },
         { name: this.$t("common.firstName"), key: "firstName" },
         { name: this.$t("common.lastName"), key: "lastName" },
-        // { name: this.$t("dashboard.study.groupId"), key: "group", filter: this.groupFilterOptions },
         { name: this.$t("common.createdAt"), key: "createdAt" },
       ];
     },
